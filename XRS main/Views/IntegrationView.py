219da5fbb79393232d3c64126d13a96a08e803ee
Clemens Prescher <clemens.prescher@gmail.com>
2014-05-15 16:32:07 -0500

    Refactoring

--- XRS main/Views/IntegrationView.py
+++ XRS main/Views/IntegrationView.py
@@ -21,7 +21,6 @@ class IntegrationView(QtGui.QWidget, Ui_xrs_integration_widget):
         self.vertical_splitter.setStretchFactor(1, 1)
         self.vertical_splitter.setSizes([100, 700])
         self.img_view = IntegrationImgView(self.img_pg_layout, orientation='horizontal')
-        self.img_view.add_mouse_move_observer(self.show_img_mouse_position)
         self.spectrum_view = SpectrumView(self.spectrum_pg_layout)
         self.set_validator()
         self.set_correct_labels()
@@ -34,21 +33,6 @@ class IntegrationView(QtGui.QWidget, Ui_xrs_integration_widget):
 
     def set_correct_labels(self):
         self.spec_unit_tth_rb.setText(u'2θ (°)')
-
-
-    def show_img_mouse_position(self, x, y):
-        try:
-            if x > 0 and y > 0:
-                x_pos_string = 'X:  %4d' % x
-                y_pos_string = 'Y:  %4d' % y
-                self.x_lbl.setText(x_pos_string)
-                self.y_lbl.setText(y_pos_string)
-
-                int_string = 'I:   %5d' % self.img_view.img_data[np.floor(x), np.floor(y)]
-                self.int_lbl.setText(int_string)
-
-        except (IndexError, AttributeError):
-            pass
 
     def switch_to_cake(self):
         self.img_view.img_view_box.setAspectLocked(False)
