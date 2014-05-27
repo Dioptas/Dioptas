dacafe9f098614796e9688dadb2fd9e891a8e1f2
Clemens Prescher <clemens.prescher@gmail.com>
2014-05-27 13:59:32 -0500

    Calibration:
    Addition of the mean factor for the refinement
    
    Some cosmetic changes to the gui

--- PDX main/UiFiles/IntegrationUI.py
+++ PDX main/UiFiles/IntegrationUI.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'Integration.ui'
 #
-# Created: Sun May 25 12:31:53 2014
+# Created: Tue May 27 13:21:37 2014
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
