c40f08265f39a9df0daced4a00ac69a28088d592
Clemens Prescher <clemens.prescher@gmail.com>
2014-05-29 23:45:32 -0500

    Added GPL3 license. Added a README.md

--- PDX main/XRS.py
+++ PDX main/XRS.py
@@ -1,3 +1,21 @@
+# -*- coding: utf8 -*-
+#     Py2DeX - GUI program for fast processing of 2D X-ray data
+#     Copyright (C) 2014  Clemens Prescher (clemens.prescher@gmail.com)
+#     GSECARS, University of Chicago
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 __author__ = 'Clemens Prescher'
 import sys
 from PyQt4 import QtGui
@@ -5,9 +23,12 @@ from Controller.MainController import MainController
 
 
 if __name__ == "__main__":
+    import os
+
     app = QtGui.QApplication(sys.argv)
-    app.setStyle('plastique')
-    # possible values:
-    # "windows", "motif", "cde", "plastique", "windowsxp", or "macintosh"
+    if os.name is not 'posix':
+        app.setStyle('plastique')
+        # possible values:
+        # "windows", "motif", "cde", "plastique", "windowsxp", or "macintosh"
     controller = MainController()
     app.exec_()
