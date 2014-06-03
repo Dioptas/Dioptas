5af66c26e1689e82c8d5c56695bfdc0447771e75
Clemens Prescher <clemens.prescher@gmail.com>
2014-06-03 02:16:53 -0500

    Clearing TODO list:
    tab order in calibration window is now ok

--- PDX main/UiFiles/IntegrationUI.py
+++ PDX main/UiFiles/IntegrationUI.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'Integration.ui'
 #
-# Created: Tue Jun  3 01:29:35 2014
+# Created: Tue Jun  3 02:16:12 2014
 # by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
--- PDX main/UiFiles/MaskUI.py
+++ PDX main/UiFiles/MaskUI.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'Mask.ui'
 #
-# Created: Tue Jun  3 01:29:35 2014
+# Created: Tue Jun  3 02:16:13 2014
 # by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
