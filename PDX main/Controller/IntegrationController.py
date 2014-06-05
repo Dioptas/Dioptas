2e20ff73745c702c7df801234cea17c20df02164
Clemens Prescher <clemens.prescher@gmail.com>
2014-06-05 15:52:49 -0600

    small fix on the integration GUI

--- PDX main/Controller/IntegrationController.py
+++ PDX main/Controller/IntegrationController.py
@@ -99,6 +99,6 @@ if __name__ == "__main__":
     controller.spectrum_controller._working_dir = '../ExampleData/spectra'
     controller.mask_data.set_dimension(controller.img_data.get_img_data().shape)
     controller.overlay_controller.add_overlay('../ExampleData/spectra/Mg2SiO4_ambient_005.xy')
-    controller.calibration_data.load('../ExampleData/test.poni')
+    controller.calibration_data.load('../ExampleData/LaB6_p49_001.poni')
     controller.image_controller.load_file_btn_click('../ExampleData/Mg2SiO4_ambient_001.tif')
     app.exec_()
