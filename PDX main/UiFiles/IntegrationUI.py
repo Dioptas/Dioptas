0168c127b3264334dbda10da1c88d372d713edc6
Clemens Prescher <clemens.prescher@gmail.com>
2014-06-05 23:20:56 -0500

    Working on Linux Compatibility:
    progress

--- PDX main/UiFiles/IntegrationUI.py
+++ PDX main/UiFiles/IntegrationUI.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'Integration.ui'
 #
-# Created: Thu Jun  5 18:24:46 2014
+# Created: Thu Jun  5 23:17:22 2014
 # by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
