047ed34720a7f96ad084d92ddeca56e2a1974797
Clemens Prescher <clemens.prescher@gmail.com>
2014-08-06 15:16:40 -0500

    Changed the colors for tablewidget in the integration view.
    it should now be possible to see the checkbox crosses there more easily

--- Dioptas_main/Views/UiFiles/MaskUI.py
+++ Dioptas_main/Views/UiFiles/MaskUI.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'Mask.ui'
 #
-# Created: Wed Aug  6 13:51:02 2014
-#      by: PyQt4 UI code generator 4.10.4
+# Created: Wed Aug 06 15:14:27 2014
+#      by: PyQt4 UI code generator 4.9.6
 #
 # WARNING! All changes made in this file will be lost!
 
