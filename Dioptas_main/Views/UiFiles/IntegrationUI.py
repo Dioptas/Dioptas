9583fb9a6a8ac6285668bac6cfaa5011a0ea7d52
Clemens Prescher <clemens.prescher@gmail.com>
2014-08-28 21:23:47 -0500

    Bugfixing. Changed JcpdsWidget againg to Qwidget because QDIalog was creating new reflection everytime you pressed enter in an input widget...

--- Dioptas_main/Views/UiFiles/IntegrationUI.py
+++ Dioptas_main/Views/UiFiles/IntegrationUI.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'Integration.ui'
 #
-# Created: Thu Aug 28 15:21:46 2014
+# Created: Thu Aug 28 21:21:10 2014
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
