1504bf8d0b28c6ffafdd748d04e1e792596692b7
Clemens Prescher <clemens.prescher@gmail.com>
2015-01-14 11:53:18 -0600

    Making the tests work again

--- Dioptas_main/Tests/test_OverlayController.py
+++ Dioptas_main/Tests/test_OverlayController.py
@@ -31,6 +31,7 @@ class OverlayControllerTest(unittest.TestCase):
         self.app = QtGui.QApplication(sys.argv)
         self.image_data = ImgData()
         self.controller = MainController()
+        self.controller.view.show()
         self.controller.calibration_controller.load_calibration(
             'Data/LaB6_p49_40keV_006.poni')
         self.controller.view.tabWidget.setCurrentIndex(2)
