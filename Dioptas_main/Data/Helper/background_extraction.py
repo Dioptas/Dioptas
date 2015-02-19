e1af95449c2c45267f5f5c3621a3ce8d331d3297
Clemens Prescher <clemens.prescher@gmail.com>
2015-02-19 12:26:30 -0600

    speeded up the bruckner smoothing. Tried also cython implementation but speedup compared to pure python was only about 5-10% so just stick with the python implementation...

--- Dioptas_main/Data/Helper/background_extraction.py
+++ Dioptas_main/Data/Helper/background_extraction.py
@@ -14,7 +14,7 @@ def extract_background(x, y, smooth_width=0.1, iterations=50, cheb_order=50):
     :param cheb_order: order of the fitted chebyshev polynomial
     :return: vector of extracted y background
     """
-    smooth_points = int((float(smooth_width)/(x[1]-x[0])))
+    smooth_points = int((float(smooth_width) / (x[1] - x[0])))
 
     y_smooth = smooth_bruckner(x, y, smooth_points, iterations)
 
@@ -26,19 +26,17 @@ def extract_background(x, y, smooth_width=0.1, iterations=50, cheb_order=50):
 
     return np.polynomial.chebyshev.chebval(x_cheb, cheb_parameters)
 
-
 def smooth_bruckner(x, y, smooth_points, iterations):
-    print smooth_points
     y_original = y
 
     N_data = y.size
     N = smooth_points
+    N_float = float(N)
     y = np.empty(N_data + N + N)
 
     y[N:N + N_data] = y_original[0:N_data]
     y[0:N].fill(y_original[N])
     y[N + N_data:N_data + N + N].fill(y_original[-1])
-    y_new = y
 
     y_avg = np.average(y)
     y_min = np.min(y)
@@ -47,9 +45,22 @@ def smooth_bruckner(x, y, smooth_points, iterations):
 
     y[y > y_c] = y_c
 
+    window_size = N_float*2+1
+
     for j in range(0, iterations):
+        window_avg = np.average(y[0: 2*N + 1])
         for i in range(N, N_data - 1 - N - 1):
-            y_new[i] = np.min([y[i], np.average(y[i - N:i + N + 1])])
-            y = y_new
+            if y[i]>window_avg:
+                y_new = window_avg
+            else:
+                y_new = y[i]
+
+            #updating central value in average
+            window_avg += (y_new-y[i])/window_size
+
+            #shift new average by one
+            window_avg += (y[i+N+1]-y[i - N])/window_size
+
+            y[i] = y_new
 
     return y[N:N + N_data]
