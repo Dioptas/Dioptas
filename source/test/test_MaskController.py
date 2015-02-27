15bb16b10cc292ffe69be301047295f3816a6507
Clemens Prescher <clemens.prescher@gmail.com>
2015-02-27 12:47:47 -0600

    renaming to model/widget in MaskController Test

--- source/test/test_MaskController.py
+++ source/test/test_MaskController.py
@@ -22,16 +22,16 @@ class MaskControllerTest(unittest.TestCase):
         self.app = QtGui.QApplication(sys.argv)
         self.working_dir = {}
 
-        self.img_data = ImgModel()
-        self.mask_data = MaskModel()
-        self.mask_view = MaskWidget()
-        self.mask_controller = MaskController(self.working_dir, self.mask_view, self.img_data, self.mask_data)
+        self.img_model = ImgModel()
+        self.mask_model = MaskModel()
+        self.mask_widget = MaskWidget()
+        self.mask_controller = MaskController(self.working_dir, self.mask_widget, self.img_model, self.mask_model)
 
     def tearDown(self):
-        del self.img_data
-        del self.mask_data
-        self.mask_view.close()
-        del self.mask_view
+        del self.img_model
+        del self.mask_model
+        self.mask_widget.close()
+        del self.mask_widget
         del self.mask_controller
         del self.app
         gc.collect()
@@ -43,32 +43,32 @@ class MaskControllerTest(unittest.TestCase):
 
     def test_loading_and_saving_mask_files(self):
         self.mask_controller.load_mask_btn_click('Data/test.mask')
-        self.mask_data.mask_below_threshold(self.img_data, 1)
+        self.mask_model.mask_below_threshold(self.img_model, 1)
         self.mask_controller.save_mask_btn_click('Data/test2.mask')
         self.assertTrue(os.path.exists('Data/test2.mask'))
         os.remove('Data/test2.mask')
 
     def test_loading_and_saving_with_super_sampling(self):
-        self.mask_data.set_supersampling(2)
+        self.mask_model.set_supersampling(2)
         self.mask_controller.load_mask_btn_click('Data/test.mask')
 
-        self.assertEqual(self.mask_data.get_mask().shape[0], 4096)
-        self.assertEqual(self.mask_data.get_mask().shape[1], 4096)
+        self.assertEqual(self.mask_model.get_mask().shape[0], 4096)
+        self.assertEqual(self.mask_model.get_mask().shape[1], 4096)
 
-        self.assertEqual(self.mask_data.get_img().shape[0], 2048)
-        self.assertEqual(self.mask_data.get_img().shape[1], 2048)
+        self.assertEqual(self.mask_model.get_img().shape[0], 2048)
+        self.assertEqual(self.mask_model.get_img().shape[1], 2048)
 
-        self.mask_data.mask_below_threshold(self.img_data, 1)
+        self.mask_model.mask_below_threshold(self.img_model, 1)
         self.mask_controller.save_mask_btn_click('Data/test1.mask')
         self.assertEqual(self.get_file_size('Data/test1.mask'), self.get_file_size('Data/test.mask'))
 
     def test_grow_and_shrinking(self):
-        self.mask_data.mask_ellipse(100,100,20,20)
-        previous_mask = np.copy(self.mask_data._mask_data)
+        self.mask_model.mask_ellipse(100,100,20,20)
+        previous_mask = np.copy(self.mask_model._mask_data)
 
-        QTest.mouseClick(self.mask_view.grow_btn, QtCore.Qt.LeftButton)
-        self.assertFalse(np.array_equal(previous_mask, self.mask_data._mask_data))
+        QTest.mouseClick(self.mask_widget.grow_btn, QtCore.Qt.LeftButton)
+        self.assertFalse(np.array_equal(previous_mask, self.mask_model._mask_data))
 
-        QTest.mouseClick(self.mask_view.shrink_btn, QtCore.Qt.LeftButton)
-        self.assertTrue(np.array_equal(previous_mask, self.mask_data._mask_data))
+        QTest.mouseClick(self.mask_widget.shrink_btn, QtCore.Qt.LeftButton)
+        self.assertTrue(np.array_equal(previous_mask, self.mask_model._mask_data))
 
