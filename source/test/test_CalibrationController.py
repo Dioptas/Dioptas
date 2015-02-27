55913ae71cc9fda848b4aad3a5014a3c20e235db
Clemens Prescher <clemens.prescher@gmail.com>
2015-02-27 12:36:56 -0600

    renaming objects to model and widget in calibration controller test

--- source/test/test_CalibrationController.py
+++ source/test/test_CalibrationController.py
@@ -18,30 +18,20 @@ from widgets.CalibrationWidget import CalibrationWidget
 class CalibrationControllerTest(unittest.TestCase):
     def setUp(self):
         self.app = QtGui.QApplication(sys.argv)
-        self.img_data = ImgModel()
-        self.mask_data = MaskModel()
-        self.calibration_data = CalibrationModel(self.img_data)
-        self.calibration_data._calibrants_working_dir = 'Data/Calibrants'
-        self.calibration_view = CalibrationWidget()
-
+        self.img_model = ImgModel()
+        self.mask_model = MaskModel()
+        self.calibration_model = CalibrationModel(self.img_model)
+        self.calibration_model._calibrants_working_dir = 'Data/Calibrants'
+        self.calibration_widget = CalibrationWidget()
         self.working_dir = {}
-
         self.calibration_controller = CalibrationController(working_dir=self.working_dir,
-                                                            img_data=self.img_data,
-                                                            mask_data=self.mask_data,
-                                                            view=self.calibration_view,
-                                                            calibration_data=self.calibration_data)
+                                                            img_data=self.img_model,
+                                                            mask_data=self.mask_model,
+                                                            view=self.calibration_widget,
+                                                            calibration_data=self.calibration_model)
 
     def tearDown(self):
-        del self.calibration_controller
-        del self.calibration_view
-        del self.calibration_data.cake_geometry
-        del self.calibration_data.spectrum_geometry
-        del self.calibration_data
-        del self.mask_data
-        del self.img_data
         del self.app
-        gc.collect()
 
     def test_automatic_calibration1(self):
         self.calibration_controller.load_img('Data/LaB6_p49_40keV_006.tif')
@@ -69,9 +59,9 @@ class CalibrationControllerTest(unittest.TestCase):
 
     def test_automatic_calibration3(self):
         self.calibration_controller.load_img('Data/CeO2_Oct24_2014_001_0000.tif')
-        QTest.mouseClick(self.calibration_view.automatic_peak_num_inc_cb, QtCore.Qt.LeftButton)
+        QTest.mouseClick(self.calibration_widget.automatic_peak_num_inc_cb, QtCore.Qt.LeftButton)
 
-        self.assertFalse(self.calibration_view.automatic_peak_num_inc_cb.isChecked())
+        self.assertFalse(self.calibration_widget.automatic_peak_num_inc_cb.isChecked())
         self.calibration_controller.search_peaks(517.664434674, 647.529865592)
         self.calibration_controller.search_peaks(667.380513299, 525.252854758)
         self.calibration_controller.search_peaks(671.110095329, 473.571503774)
@@ -85,15 +75,15 @@ class CalibrationControllerTest(unittest.TestCase):
         self.calibration_controller.view.sv_pixel_height_txt.setText('172')
         self.calibration_controller.view.calibrant_cb.setCurrentIndex(4)
 
-        start_values = self.calibration_view.get_start_values()
+        start_values = self.calibration_widget.get_start_values()
         self.assertAlmostEqual(start_values['wavelength'], 0.406626e-10)
         self.assertAlmostEqual(start_values['pixel_height'], 172e-6)
         self.assertAlmostEqual(start_values['pixel_width'], 172e-6)
         self.calibration_controller.load_calibrant()
-        self.assertAlmostEqual(self.calibration_data.calibrant.wavelength, 0.406626e-10)
+        self.assertAlmostEqual(self.calibration_model.calibrant.wavelength, 0.406626e-10)
 
-        QTest.mouseClick(self.calibration_view.integrate_btn, QtCore.Qt.LeftButton)
-        calibration_parameter = self.calibration_data.get_calibration_parameter()[0]
+        QTest.mouseClick(self.calibration_widget.integrate_btn, QtCore.Qt.LeftButton)
+        calibration_parameter = self.calibration_model.get_calibration_parameter()[0]
 
         self.assertAlmostEqual(calibration_parameter['dist'], .2086, places=4)
 
@@ -106,8 +96,8 @@ class CalibrationControllerTest(unittest.TestCase):
         self.calibration_controller.view.sv_pixel_width_txt.setText('79')
         self.calibration_controller.view.sv_pixel_height_txt.setText('79')
         self.calibration_controller.view.calibrant_cb.setCurrentIndex(7)
-        self.img_data.set_supersampling(2)
-        self.calibration_data.set_supersampling(2)
+        self.img_model.set_supersampling(2)
+        self.calibration_model.set_supersampling(2)
         self.calibration_controller.calibrate()
 
     def test_automatic_calibration_with_supersampling_and_mask(self):
@@ -119,10 +109,10 @@ class CalibrationControllerTest(unittest.TestCase):
         self.calibration_controller.view.sv_pixel_width_txt.setText('79')
         self.calibration_controller.view.sv_pixel_height_txt.setText('79')
         self.calibration_controller.view.calibrant_cb.setCurrentIndex(7)
-        self.img_data.set_supersampling(2)
-        self.mask_data.mask_below_threshold(self.img_data._img_data, 1)
-        self.mask_data.set_supersampling(2)
-        self.calibration_data.set_supersampling(2)
+        self.img_model.set_supersampling(2)
+        self.mask_model.mask_below_threshold(self.img_model._img_data, 1)
+        self.mask_model.set_supersampling(2)
+        self.calibration_model.set_supersampling(2)
         self.calibration_controller.view.use_mask_cb.setChecked(True)
         self.calibration_controller.calibrate()
 
@@ -138,11 +128,11 @@ class CalibrationControllerTest(unittest.TestCase):
         self.calibration_controller.view.options_automatic_refinement_cb.setChecked(False)
         self.calibration_controller.view.use_mask_cb.setChecked(True)
         self.calibration_controller.calibrate()
-        self.calibration_data.integrate_1d()
-        self.calibration_data.integrate_2d()
+        self.calibration_model.integrate_1d()
+        self.calibration_model.integrate_2d()
         self.calibration_controller.load_img('Data/CeO2_Pilatus1M.tif')
-        self.calibration_data.integrate_1d()
-        self.calibration_data.integrate_2d()
+        self.calibration_model.integrate_1d()
+        self.calibration_model.integrate_2d()
 
     def test_loading_and_saving_of_calibration_files(self):
         self.calibration_controller.load_calibration('Data/calibration.poni')
