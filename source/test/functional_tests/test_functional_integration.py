05666975fc0d52e8cdda6689a9528e15dee60fe0
Clemens Prescher <clemens.prescher@gmail.com>
2015-02-27 14:37:41 -0600

    moved functional test into their respective folders and changed the paths to the data files accordingly

--- source/test/functional_tests/test_functional_integration.py
+++ source/test/functional_tests/test_functional_integration.py
@@ -9,11 +9,12 @@ import gc
 from PyQt4 import QtGui, QtCore
 from PyQt4.QtTest import QTest
 
-from controller.MainController import MainController
 from controller.integration import IntegrationController
 from model import ImgModel, CalibrationModel, MaskModel, SpectrumModel, PhaseModel
 from widgets.IntegrationWidget import IntegrationWidget
 
+unittest_path = os.path.dirname(__file__)
+data_path = os.path.join(unittest_path, os.pardir, 'data')
 
 class IntegrationFunctionalTest(unittest.TestCase):
     def setUp(self):
@@ -33,8 +34,8 @@ class IntegrationFunctionalTest(unittest.TestCase):
                                                               calibration_model=self.calibration_model,
                                                               spectrum_model=self.spectrum_model,
                                                               phase_model=self.phase_model)
-        self.calibration_model.load('Data/CeO2_Pilatus1M.poni')
-        self.img_model.load('Data/CeO2_Pilatus1M.tif')
+        self.calibration_model.load(os.path.join(data_path, 'CeO2_Pilatus1M.poni'))
+        self.img_model.load(os.path.join(data_path, 'CeO2_Pilatus1M.tif'))
 
 
         self.integration_spectrum_controller = self.integration_controller.spectrum_controller
@@ -104,47 +105,47 @@ class IntegrationFunctionalTest(unittest.TestCase):
         self.assertEqual(self.img_model.img_data.shape[0], 2 * img_shape[0])
         self.assertEqual(self.img_model.img_data.shape[1], 2 * img_shape[1])
 
-        self.mask_model.load_mask('Data/test.mask')
+        self.mask_model.load_mask(os.path.join(data_path,'test.mask'))
         QTest.mouseClick(self.integration_widget.img_mask_btn, QtCore.Qt.LeftButton)
         QTest.mouseClick(self.integration_widget.img_mode_btn, QtCore.Qt.LeftButton)
 
     def test_saving_image(self):
         # Tests if the image save procedures are working for the different possible file endings
-        self.integration_image_controller.save_img('Data/Test_img.png')
-        self.integration_image_controller.save_img('Data/Test_img.tiff')
+        self.integration_image_controller.save_img(os.path.join(data_path, 'Test_img.png'))
+        self.integration_image_controller.save_img(os.path.join(data_path, 'Test_img.tiff'))
 
-        self.assertTrue(os.path.exists('Data/Test_img.png'))
-        self.assertTrue(os.path.exists('Data/Test_img.tiff'))
+        self.assertTrue(os.path.exists(os.path.join(data_path, 'Test_img.png')))
+        self.assertTrue(os.path.exists(os.path.join(data_path, 'Test_img.tiff')))
 
-        os.remove('Data/Test_img.png')
-        os.remove('Data/Test_img.tiff')
+        os.remove(os.path.join(data_path, 'Test_img.png'))
+        os.remove(os.path.join(data_path, 'Test_img.tiff'))
 
     def test_saving_spectrum(self):
         # Tests if the spectrum save procedures is are working for all fileendings
         def save_spectra_test_for_size_and_delete(self):
-            self.integration_spectrum_controller.save_spectrum('Data/Test_spec.xy')
-            self.integration_spectrum_controller.save_spectrum('Data/Test_spec.chi')
-            self.integration_spectrum_controller.save_spectrum('Data/Test_spec.dat')
-            self.integration_spectrum_controller.save_spectrum('Data/Test_spec.png')
-            self.integration_spectrum_controller.save_spectrum('Data/Test_spec.svg')
+            self.integration_spectrum_controller.save_spectrum(os.path.join(data_path, 'Test_spec.xy'))
+            self.integration_spectrum_controller.save_spectrum(os.path.join(data_path, 'Test_spec.chi'))
+            self.integration_spectrum_controller.save_spectrum(os.path.join(data_path, 'Test_spec.dat'))
+            self.integration_spectrum_controller.save_spectrum(os.path.join(data_path, 'Test_spec.png'))
+            self.integration_spectrum_controller.save_spectrum(os.path.join(data_path, 'Test_spec.svg'))
 
-            self.assertTrue(os.path.exists('Data/Test_spec.xy'))
-            self.assertTrue(os.path.exists('Data/Test_spec.chi'))
-            self.assertTrue(os.path.exists('Data/Test_spec.dat'))
-            self.assertTrue(os.path.exists('Data/Test_spec.png'))
-            self.assertTrue(os.path.exists('Data/Test_spec.svg'))
+            self.assertTrue(os.path.exists(os.path.join(data_path, 'Test_spec.xy')))
+            self.assertTrue(os.path.exists(os.path.join(data_path, 'Test_spec.chi')))
+            self.assertTrue(os.path.exists(os.path.join(data_path, 'Test_spec.dat')))
+            self.assertTrue(os.path.exists(os.path.join(data_path, 'Test_spec.png')))
+            self.assertTrue(os.path.exists(os.path.join(data_path, 'Test_spec.svg')))
 
-            self.assertGreater(os.stat('Data/Test_spec.xy').st_size, 1)
-            self.assertGreater(os.stat('Data/Test_spec.chi').st_size, 1)
-            self.assertGreater(os.stat('Data/Test_spec.dat').st_size, 1)
-            self.assertGreater(os.stat('Data/Test_spec.png').st_size, 1)
-            self.assertGreater(os.stat('Data/Test_spec.svg').st_size, 1)
+            self.assertGreater(os.stat(os.path.join(data_path, 'Test_spec.xy')).st_size, 1)
+            self.assertGreater(os.stat(os.path.join(data_path, 'Test_spec.chi')).st_size, 1)
+            self.assertGreater(os.stat(os.path.join(data_path, 'Test_spec.dat')).st_size, 1)
+            self.assertGreater(os.stat(os.path.join(data_path, 'Test_spec.png')).st_size, 1)
+            self.assertGreater(os.stat(os.path.join(data_path, 'Test_spec.svg')).st_size, 1)
 
-            os.remove('Data/Test_spec.xy')
-            os.remove('Data/Test_spec.chi')
-            os.remove('Data/Test_spec.dat')
-            os.remove('Data/Test_spec.png')
-            os.remove('Data/Test_spec.svg')
+            os.remove(os.path.join(data_path, 'Test_spec.xy'))
+            os.remove(os.path.join(data_path, 'Test_spec.chi'))
+            os.remove(os.path.join(data_path, 'Test_spec.dat'))
+            os.remove(os.path.join(data_path, 'Test_spec.png'))
+            os.remove(os.path.join(data_path, 'Test_spec.svg'))
 
         save_spectra_test_for_size_and_delete(self)
         QTest.mouseClick(self.integration_spectrum_controller.widget.spec_q_btn, QtCore.Qt.LeftButton)
