c06692b79aa62c02f12a942bb677882c00c54315
Clemens Prescher <clemens.prescher@gmail.com>
2015-02-27 12:46:47 -0600

    renaming variables in JcpdsEditorWidget Test

--- source/test/test_JcpdsEditorWidget.py
+++ source/test/test_JcpdsEditorWidget.py
@@ -23,72 +23,72 @@ from PyQt4 import QtGui
 import sys
 import gc
 
-class JcpdsDisplayTestAuAnderson(unittest.TestCase):
+class JcpdsEditorTest(unittest.TestCase):
     def setUp(self):
         self.app = QtGui.QApplication(sys.argv)
         self.jcpds = jcpds()
         self.jcpds.compute_v0()
         self.jcpds.load_file('Data/jcpds/au_Anderson.jcpds')
 
-        self.jcpds_editor = JcpdsEditorWidget()
-        self.jcpds_editor.show_jcpds(self.jcpds, wavelength=0.31)
+        self.jcpds_editor_widget = JcpdsEditorWidget()
+        self.jcpds_editor_widget.show_jcpds(self.jcpds, wavelength=0.31)
 
     def tearDown(self):
         del self.jcpds
-        self.jcpds_editor.close()
-        del self.jcpds_editor
+        self.jcpds_editor_widget.close()
+        del self.jcpds_editor_widget
         del self.app
         gc.collect()
 
     def test_filename_and_comment_are_shown_correctly(self):
-        self.assertEqual(self.jcpds_editor.filename_txt.text(),
+        self.assertEqual(self.jcpds_editor_widget.filename_txt.text(),
                          self.jcpds.filename)
-        self.assertEqual(self.jcpds_editor.comments_txt.text(),
+        self.assertEqual(self.jcpds_editor_widget.comments_txt.text(),
                          self.jcpds.comments[0])
 
     def test_all_lattice_parameters_are_shown_correctly(self):
-        self.assertEqual(self.jcpds_editor.lattice_a_sb.value(),
+        self.assertEqual(self.jcpds_editor_widget.lattice_a_sb.value(),
                          self.jcpds.a0)
-        self.assertEqual(self.jcpds_editor.lattice_b_sb.value(),
+        self.assertEqual(self.jcpds_editor_widget.lattice_b_sb.value(),
                          self.jcpds.b0)
-        self.assertEqual(self.jcpds_editor.lattice_c_sb.value(),
+        self.assertEqual(self.jcpds_editor_widget.lattice_c_sb.value(),
                          self.jcpds.c0)
-        self.assertAlmostEqual(float(str(self.jcpds_editor.lattice_volume_txt.text())),
+        self.assertAlmostEqual(float(str(self.jcpds_editor_widget.lattice_volume_txt.text())),
                          self.jcpds.v0, delta=0.0001)
 
-        self.assertEqual(float(str(self.jcpds_editor.lattice_eos_a_txt.text())),
+        self.assertEqual(float(str(self.jcpds_editor_widget.lattice_eos_a_txt.text())),
                          self.jcpds.a)
-        self.assertEqual(float(str(self.jcpds_editor.lattice_eos_b_txt.text())),
+        self.assertEqual(float(str(self.jcpds_editor_widget.lattice_eos_b_txt.text())),
                          self.jcpds.b)
-        self.assertEqual(float(str(self.jcpds_editor.lattice_eos_c_txt.text())),
+        self.assertEqual(float(str(self.jcpds_editor_widget.lattice_eos_c_txt.text())),
                          self.jcpds.c)
 
-        self.assertEqual(self.jcpds_editor.lattice_alpha_sb.value(),
+        self.assertEqual(self.jcpds_editor_widget.lattice_alpha_sb.value(),
                          self.jcpds.alpha)
-        self.assertEqual(self.jcpds_editor.lattice_beta_sb.value(),
+        self.assertEqual(self.jcpds_editor_widget.lattice_beta_sb.value(),
                          self.jcpds.beta0)
-        self.assertEqual(self.jcpds_editor.lattice_gamma_sb.value(),
+        self.assertEqual(self.jcpds_editor_widget.lattice_gamma_sb.value(),
                          self.jcpds.gamma0)
 
-        self.assertEqual(self.jcpds_editor.lattice_ab_sb.value(),
+        self.assertEqual(self.jcpds_editor_widget.lattice_ab_sb.value(),
                          self.jcpds.a0/float(self.jcpds.b0))
-        self.assertEqual(self.jcpds_editor.lattice_ca_sb.value(),
+        self.assertEqual(self.jcpds_editor_widget.lattice_ca_sb.value(),
                          1)
-        self.assertEqual(self.jcpds_editor.lattice_cb_sb.value(),
+        self.assertEqual(self.jcpds_editor_widget.lattice_cb_sb.value(),
                          1)
 
     def test_all_eos_parameters_are_shown_correctly(self):
-        self.assertEqual(float(str(self.jcpds_editor.eos_K_txt.text())),
+        self.assertEqual(float(str(self.jcpds_editor_widget.eos_K_txt.text())),
                          self.jcpds.k0)
-        self.assertEqual(float(str(self.jcpds_editor.eos_Kp_txt.text())),
+        self.assertEqual(float(str(self.jcpds_editor_widget.eos_Kp_txt.text())),
                          self.jcpds.k0p0)
-        self.assertEqual(float(str(self.jcpds_editor.eos_alphaT_txt.text())),
+        self.assertEqual(float(str(self.jcpds_editor_widget.eos_alphaT_txt.text())),
                          self.jcpds.alpha_t0)
-        self.assertEqual(float(str(self.jcpds_editor.eos_dalphadT_txt.text())),
+        self.assertEqual(float(str(self.jcpds_editor_widget.eos_dalphadT_txt.text())),
                          self.jcpds.d_alpha_dt)
-        self.assertEqual(float(str(self.jcpds_editor.eos_dKdT_txt.text())),
+        self.assertEqual(float(str(self.jcpds_editor_widget.eos_dKdT_txt.text())),
                          self.jcpds.dk0dt)
-        self.assertEqual(float(str(self.jcpds_editor.eos_dKpdT_txt.text())),
+        self.assertEqual(float(str(self.jcpds_editor_widget.eos_dKpdT_txt.text())),
                          self.jcpds.dk0pdt)
 
 
