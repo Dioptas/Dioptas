ab2b08e40d80b825e6a30be3ca218b10ea8f65d5
Clemens Prescher <clemens.prescher@gmail.com>
2015-02-27 12:45:53 -0600

    renaming variables IntegrationBackgroundControllerTest to model/widget

--- source/test/test_IntegrationBackgroundController.py
+++ source/test/test_IntegrationBackgroundController.py
@@ -34,60 +34,60 @@ from controller.integration import SpectrumController
 class IntegrationBackgroundControllerTest(unittest.TestCase):
     def setUp(self):
         self.app = QtGui.QApplication(sys.argv)
-        self.view = IntegrationWidget()
-        self.spectrum_data = SpectrumModel()
-        self.img_data = ImgModel()
-        self.spectrum_controller = SpectrumController({}, self.view, self.img_data,
-                                                                   None, None, self.spectrum_data)
-        self.controller = BackgroundController({}, self.view, self.img_data, self.spectrum_data)
-        self.overlay_tw = self.view.overlay_tw
+        self.widget = IntegrationWidget()
+        self.spectrum_model = SpectrumModel()
+        self.img_model = ImgModel()
+        self.spectrum_controller = SpectrumController({}, self.widget, self.img_model,
+                                                                   None, None, self.spectrum_model)
+        self.background_controller = BackgroundController({}, self.widget, self.img_model, self.spectrum_model)
+        self.overlay_tw = self.widget.overlay_tw
 
     def tearDown(self):
         del self.app
 
     def test_spectrum_bkg_toggle_inspect_button(self):
-        self.spectrum_data.load_spectrum(os.path.join('Data', 'FoG_D3_001.xy'))
-        self.view.bkg_spectrum_gb.setChecked(True)
-        self.view.bkg_spectrum_inspect_btn.toggle()
-        x_bkg, y_bkg = self.view.spectrum_view.bkg_item.getData()
+        self.spectrum_model.load_spectrum(os.path.join('Data', 'FoG_D3_001.xy'))
+        self.widget.bkg_spectrum_gb.setChecked(True)
+        self.widget.bkg_spectrum_inspect_btn.toggle()
+        x_bkg, y_bkg = self.widget.spectrum_view.bkg_item.getData()
         self.assertGreater(len(x_bkg), 0)
 
-        self.view.bkg_spectrum_inspect_btn.toggle()
-        x_bkg, y_bkg = self.view.spectrum_view.bkg_item.getData()
+        self.widget.bkg_spectrum_inspect_btn.toggle()
+        x_bkg, y_bkg = self.widget.spectrum_view.bkg_item.getData()
         self.assertEqual(len(x_bkg), 0)
 
     def test_spectrum_bkg_inspect_btn_is_untoggled_when_disabling_spectrum_gb(self):
-        self.spectrum_data.load_spectrum(os.path.join('Data', 'FoG_D3_001.xy'))
-        self.view.bkg_spectrum_gb.setChecked(True)
-        self.view.bkg_spectrum_inspect_btn.toggle()
-        self.view.bkg_spectrum_gb.setChecked(False)
-        x_bkg, y_bkg = self.view.spectrum_view.bkg_item.getData()
+        self.spectrum_model.load_spectrum(os.path.join('Data', 'FoG_D3_001.xy'))
+        self.widget.bkg_spectrum_gb.setChecked(True)
+        self.widget.bkg_spectrum_inspect_btn.toggle()
+        self.widget.bkg_spectrum_gb.setChecked(False)
+        x_bkg, y_bkg = self.widget.spectrum_view.bkg_item.getData()
         self.assertEqual(len(x_bkg), 0)
 
     def test_spectrum_bkg_linear_region_changes_txt_fields(self):
-        self.spectrum_data.load_spectrum(os.path.join('Data', 'FoG_D3_001.xy'))
-        self.view.bkg_spectrum_gb.setChecked(True)
-        self.view.bkg_spectrum_inspect_btn.toggle()
+        self.spectrum_model.load_spectrum(os.path.join('Data', 'FoG_D3_001.xy'))
+        self.widget.bkg_spectrum_gb.setChecked(True)
+        self.widget.bkg_spectrum_inspect_btn.toggle()
 
-        self.view.spectrum_view.set_linear_region(5, 11)
+        self.widget.spectrum_view.set_linear_region(5, 11)
 
-        x_min = float(str(self.view.bkg_spectrum_x_min_txt.text()))
-        x_max = float(str(self.view.bkg_spectrum_x_max_txt.text()))
+        x_min = float(str(self.widget.bkg_spectrum_x_min_txt.text()))
+        x_max = float(str(self.widget.bkg_spectrum_x_max_txt.text()))
 
         self.assertEqual(x_min, 5)
         self.assertEqual(x_max, 11)
 
     def test_spectrum_bkg_txt_fields_change_linear_regions(self):
-        self.spectrum_data.load_spectrum(os.path.join('Data', 'FoG_D3_001.xy'))
-        self.view.bkg_spectrum_gb.setChecked(True)
-        self.view.bkg_spectrum_inspect_btn.toggle()
+        self.spectrum_model.load_spectrum(os.path.join('Data', 'FoG_D3_001.xy'))
+        self.widget.bkg_spectrum_gb.setChecked(True)
+        self.widget.bkg_spectrum_inspect_btn.toggle()
 
-        self.view.bkg_spectrum_x_min_txt.setText('5')
-        QTest.keyPress(self.view.bkg_spectrum_x_min_txt, QtCore.Qt.Key_Enter)
-        self.view.bkg_spectrum_x_max_txt.setText('11')
-        QTest.keyPress(self.view.bkg_spectrum_x_max_txt, QtCore.Qt.Key_Enter)
+        self.widget.bkg_spectrum_x_min_txt.setText('5')
+        QTest.keyPress(self.widget.bkg_spectrum_x_min_txt, QtCore.Qt.Key_Enter)
+        self.widget.bkg_spectrum_x_max_txt.setText('11')
+        QTest.keyPress(self.widget.bkg_spectrum_x_max_txt, QtCore.Qt.Key_Enter)
 
-        x_min, x_max = self.view.spectrum_view.linear_region_item.getRegion()
+        x_min, x_max = self.widget.spectrum_view.linear_region_item.getRegion()
 
         self.assertEqual(x_min, 5)
         self.assertEqual(x_max, 11)
