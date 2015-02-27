ad7ea6b0e4bad1959869e5af943a06d3cc812795
Clemens Prescher <clemens.prescher@gmail.com>
2015-02-27 14:04:57 -0600

    Updated files used by the CalibrationModelTest

--- source/test/test_CalibrationModel.py
+++ source/test/test_CalibrationModel.py
@@ -1,13 +1,16 @@
 # -*- coding: utf8 -*-
 __author__ = 'Clemens Prescher'
 
-
-
 import unittest
+import os
 
 from model.CalibrationModel import CalibrationModel
 from model.ImgModel import ImgModel
 import gc
+
+
+unittest_path = os.path.dirname(__file__)
+data_path = os.path.join(unittest_path, 'data')
 
 class CalibrationModelTest(unittest.TestCase):
     def setUp(self):
@@ -22,9 +25,9 @@ class CalibrationModelTest(unittest.TestCase):
         gc.collect()
 
     def test_loading_calibration_gives_right_pixel_size(self):
-        self.calibration_model.spectrum_geometry.load("Data/calibration_PE.poni")
-        self.assertEqual(self.calibration_model.spectrum_geometry.pixel1, 0.0002)
+        self.calibration_model.spectrum_geometry.load(os.path.join(data_path, 'CeO2_Pilatus1M.poni'))
+        self.assertEqual(self.calibration_model.spectrum_geometry.pixel1, 0.000172)
 
 
-        self.calibration_model.load("Data/calibration_PE.poni")
-        self.assertEqual(self.calibration_model.spectrum_geometry.pixel1, 0.0002)
+        self.calibration_model.load(os.path.join(data_path,'LaB6_40keV_MarCCD.poni'))
+        self.assertEqual(self.calibration_model.spectrum_geometry.pixel1, 0.000079)
