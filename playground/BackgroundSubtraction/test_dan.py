f03429c994b55dae282fc975ef9409abe8e33f9a
Clemens Prescher <clemens.prescher@gmail.com>
2015-03-02 16:55:00 -0600

    renaming the playground folder...

---
+++ playground/BackgroundSubtraction/test_dan.py
@@ -0,0 +1,21 @@
+# -*- coding: utf8 -*-
+__author__ = 'Clemens Prescher'
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from from_dan import fit_bg_cheb_auto
+
+def get_data(filename):
+    data = np.loadtxt(filename)
+    x = data[:,0]
+    y = data[:,1]
+    return x,y
+
+x,y = get_data('Fe7C3_300_020.xy')
+# x,y = get_data('test2.xy')
+
+background = fit_bg_cheb_auto(x, y, 10,20,100)
+plt.plot(x,y)
+plt.plot(x,background)
+plt.show()
