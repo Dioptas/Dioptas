4fbb1efe09dd770b4d5d8091253fe8425a3e869f
Clemens Prescher <clemens.prescher@gmail.com>
2015-04-06 11:12:25 -0500

    fixed a bug introduced by an extra calculation in the smooth bruckner function...

--- dioptas/model/Helper/smooth_bruckner.f95
+++ dioptas/model/Helper/smooth_bruckner.f95
@@ -18,29 +18,15 @@ double precision, intent(out), dimension(0:n-1) :: y_bkg
 
 double precision, dimension(0:n+2*smooth_points-1) :: y_extended
 
-double precision :: y_avg
 double precision :: window_avg
 double precision :: y_new 
 integer :: i, j
 double precision :: window_size
 
-double precision :: y_cutoff
-
-
 ! creating an extended y with having size n + 2*smooth_points
 y_extended(0:smooth_points-1) = y(0)
 y_extended(smooth_points:n+smooth_points-1) = y
 y_extended(smooth_points+n:2*smooth_points+n-1) = y(n-1)
-
-
-y_avg = sum(y_extended)/size(y_extended)
-y_cutoff = y_avg + 2 * (y_avg - minval(y_extended))
-
-do i=0, size(y_extended)-1
-    if (y_extended(i)>y_cutoff) then
-        y_extended(i) = y_cutoff
-    end if
-end do
 
 ! actual algorithm:
 window_size = 2*smooth_points + 1
