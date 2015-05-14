1a9c4c21ca486beb0c76106c497aa203e328d4a0
Clemens Prescher <clemens.prescher@gmail.com>
2015-05-14 12:23:20 -0500

    Cleaning up the travis output and making it a little bit more understandable

--- dioptas/test/run_tests.py
+++ dioptas/test/run_tests.py
@@ -15,6 +15,9 @@ for folder in folders:
 
 exit_codes = []
 for test_file in test_files:
+    print("##############################")
+    print("##############################")
+    print("Running: " + "python {}".format(test_file))
     exit_code = call("python {}".format(test_file), shell=True)
     exit_codes.append(exit_code)
 
