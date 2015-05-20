66b877f0411147bf7a54fa2d69e7e728b4163bca
Clemens Prescher <Clemens.Prescher@gmail.com>
2015-05-20 12:04:13 -0500

    Making functions private and creating Doc Strings for the CifConversionParametersDialog

--- dioptas/widgets/UtilityWidgets.py
+++ dioptas/widgets/UtilityWidgets.py
@@ -8,17 +8,23 @@ widget_path = os.path.dirname(__file__)
 
 
 class CifConversionParametersDialog(QtGui.QDialog):
+    """
+    Dialog which is asking for Intensity Cutoff and minimum d-spacing when loading cif files.
+    """
     def __init__(self, parent):
         super(CifConversionParametersDialog, self).__init__()
 
         self._parent = parent
-        self.create_widgets()
-        self.layout_widgets()
-        self.style_widgets()
+        self._create_widgets()
+        self._layout_widgets()
+        self._style_widgets()
 
-        self.connect_widgets()
+        self._connect_widgets()
 
-    def create_widgets(self):
+    def _create_widgets(self):
+        """
+        Creates all necessary widgets.
+        """
         self.int_cutoff_lbl = QtGui.QLabel("Intensity Cutoff:")
         self.min_d_spacing_lbl = QtGui.QLabel("Minimum d-spacing:")
 
@@ -32,7 +38,10 @@ class CifConversionParametersDialog(QtGui.QDialog):
 
         self.ok_btn = QtGui.QPushButton("OK")
 
-    def layout_widgets(self):
+    def _layout_widgets(self):
+        """
+        Layouts the widgets into a gridlayout
+        """
         self._layout = QtGui.QGridLayout()
         self._layout.addWidget(self.int_cutoff_lbl, 0, 0)
         self._layout.addWidget(self.int_cutoff_txt, 0, 1)
@@ -44,7 +53,10 @@ class CifConversionParametersDialog(QtGui.QDialog):
 
         self.setLayout(self._layout)
 
-    def style_widgets(self):
+    def _style_widgets(self):
+        """
+        Makes everything pretty and set Double validators for the line edits.
+        """
         self.int_cutoff_lbl.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
         self.int_cutoff_txt.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
         self.int_cutoff_txt.setMaximumWidth(40)
@@ -63,19 +75,30 @@ class CifConversionParametersDialog(QtGui.QDialog):
         file.close()
 
 
-    def connect_widgets(self):
+    def _connect_widgets(self):
+        """
+        Connecting actions to slots.
+        """
         self.ok_btn.clicked.connect(self.accept)
 
     @property
     def int_cutoff(self):
+        """
+        Returns the intensity cutoff selected in the dialog.
+        """
         return float(str(self.int_cutoff_txt.text()))
 
     @property
     def min_d_spacing(self):
+        """
+        Returns the minimum d-spacing selected in the dialog.
+        """
         return float(str(self.min_d_spacing_txt.text()))
 
     def exec_(self):
-          #moving conversion widget into the center of
+        """
+        Overwriting the dialog exec_ function to center the widget in the parent window before execution.
+        """
         parent_center = self._parent.window().mapToGlobal(self._parent.window().rect().center())
         self.move(parent_center.x()-101,parent_center.y()-48)
         super(CifConversionParametersDialog, self).exec_()
