1aeec884ddb376a2d9c8c19d813b0ce0762998ae
Clemens Prescher <clemens.prescher@gmail.com>
2015-11-24 08:24:51 -0600

    qt_graphicssystem is now only changed when linux is used as operating system

--- dioptas/Dioptas.py
+++ dioptas/Dioptas.py
@@ -20,8 +20,10 @@ from __future__ import absolute_import
 
 __author__ = 'Clemens Prescher'
 
-import os
-os.environ['QT_GRAPHICSSYSTEM']='native'
+from sys import platform as _platform
+if _platform == "linux" or _platform == "linux2":
+    import os
+    os.environ['QT_GRAPHICSSYSTEM']='native'
 
 import sys
 from PyQt4 import QtGui
@@ -32,11 +34,8 @@ print("Dioptas {}".format(get_version()))
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
-    from sys import platform as _platform
 
-    if _platform == "linux" or _platform == "linux2":
-        app.setStyle('plastique')
-    elif _platform == "win32" or _platform == 'cygwin':
+    if _platform == "linux" or _platform == "linux" or _platform == "win32" or _platform == 'cygwin':
         app.setStyle('plastique')
         # possible values:
         # "windows", "motif", "cde", "plastique", "windowsxp", or "macitosh"
