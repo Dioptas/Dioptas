6d48d68d9244afd7212d479e20b61ebd307553e2
Clemens Prescher <clemens.prescher@gmail.com>
2016-01-14 12:38:52 +0100

    continueing to refactor and adding licencing

--- dioptas/test/unit/test_background_extraction.py
+++ dioptas/test/unit/test_background_extraction.py
@@ -1,4 +1,19 @@
-# -*- coding: utf8 -*-
+# Dioptas - GUI program for fast processing of 2D X-ray data
+# Copyright (C) 2015  Clemens Prescher (clemens.prescher@gmail.com)
+# Institute for Geology and Mineralogy, University of Cologne
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
 import numpy as np
@@ -8,12 +23,6 @@ from model.util.PeakShapes import gaussian
 
 
 class TestBackgroundExtraction(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
     def test_simple_linear_background_with_single_peak(self):
         x = np.linspace(0, 25, 2500)
         y_data = gaussian(x, 10, 3, 0.1)
@@ -42,7 +51,6 @@ class TestBackgroundExtraction(unittest.TestCase):
         self.assertAlmostEqual(np.sum(y_data - (y_measurement - y_extracted_bkg)), 0)
 
     def test_simple_linear_background_with_multiple_close_peaks(self):
-
         x = np.linspace(0, 24, 2500)
         y_data = np.zeros(x.shape)
 
@@ -59,7 +67,3 @@ class TestBackgroundExtraction(unittest.TestCase):
 
         y_extracted_bkg = extract_background(x, y_measurement, 1)
         self.assertAlmostEqual(np.sum(y_data - (y_measurement - y_extracted_bkg)), 0)
-
-
-if __name__ == '__main__':
-    unittest.main()
