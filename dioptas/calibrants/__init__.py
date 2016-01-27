62e080c90af9034f8718f1d374cd06441347101c
Clemens Prescher <clemens.prescher@gmail.com>
2016-01-27 13:04:54 +0100

    updating the copyright text

--- dioptas/calibrants/__init__.py
+++ dioptas/calibrants/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
 # Dioptas - GUI program for fast processing of 2D X-ray data
-# Copyright (C) 2014  Clemens Prescher (clemens.prescher@gmail.com)
+# Copyright (C) 2015  Clemens Prescher (clemens.prescher@gmail.com)
 #     GSECARS, University of Chicago
 #
 #     This program is free software: you can redistribute it and/or modify
