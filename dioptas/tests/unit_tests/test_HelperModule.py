d83bbde7f20fb37994d7db2dfa09f5a46206ae50
Clemens Prescher <clemens.prescher@gmail.com>
2016-02-03 09:18:18 +0100

    fixing tests

--- dioptas/tests/unit_tests/test_HelperModule.py
+++ dioptas/tests/unit_tests/test_HelperModule.py
@@ -2,13 +2,9 @@
 
 import unittest
 import os
-import numpy as np
 
 from PyQt4 import QtGui
 
-from model.CalibrationModel import CalibrationModel
-from model.ImgModel import ImgModel
-from model.util.HelperModule import reverse_interpolate_two_array, reverse_interpolate_two_array2
 
 unittest_path = os.path.dirname(__file__)
 data_path = os.path.join(unittest_path, '../data')
