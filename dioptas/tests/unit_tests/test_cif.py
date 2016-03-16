4a727e239a6d0815fced3d0463897fed66555d03
Clemens Prescher <clemens.prescher@gmail.com>
2016-03-16 10:58:19 +0900

    working on Fe2O3 cif files from Elena

--- dioptas/tests/unit_tests/test_cif.py
+++ dioptas/tests/unit_tests/test_cif.py
@@ -92,3 +92,14 @@ class TestCifModule(unittest.TestCase):
         cif_converter = CifConverter(0.31, min_d_spacing=1.5, min_intensity=10)
         jcpds_phase = cif_converter.convert_cif_to_jcpds(os.path.join(cif_path, 'apatite.cif'))
         self.assertEqual(jcpds_phase.a0, 9.628)
+
+    def test_read_cif_from_shelx(self):
+        cif_phase = ReadCif(get_cif_url('Fe2O3_shelx.cif'))
+        real_phase = cif_phase[cif_phase.keys()[0]]
+        print real_phase['_space_group_name_h-m_alt']
+        # print '-----------------------------------------'
+        # cif_converter = CifConverter(0.31)
+        # jcpds_phase = cif_converter.convert_cif_to_jcpds(os.path.join(cif_path, 'Fe2O3_shelx.cif'))
+        # jcpds_phase['']
+
+
