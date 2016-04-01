8708b878d6043c2103dc3b5669d2d74a4e23fd1a
Clemens Prescher <clemens.prescher@gmail.com>
2016-04-01 14:41:14 +0200

    added an exception hook for errors, so they will be much easier to report for normal useres

--- dioptas/Dioptas.py
+++ dioptas/Dioptas.py
@@ -17,21 +17,71 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import
-
-from sys import platform as _platform
 import sys
+import os
+import time
+import cStringIO
+import traceback
 from PyQt4 import QtGui
 
-from controller.MainController import MainController, get_version
+app = QtGui.QApplication(sys.argv)
+app.setGraphicsSystem("raster")
 
-print("Dioptas {}".format(get_version()))
+dioptas_version = "not yet specified"
+
+
+def excepthook(exc_type, exc_value, traceback_obj):
+    """
+    Global function to catch unhandled exceptions. This function will result in an error dialog which displays the
+    error information.
+
+    :param exc_type: exception type
+    :param exc_value: exception value
+    :param traceback_obj: traceback object
+    :return:
+    """
+
+    separator = '-' * 80
+    log_file = "error.log"
+    notice = \
+        """An unhandled exception occurred. Please report the bug under:\n """ \
+        """\t%s\n""" \
+        """or via email to:\n\t <%s>.\n\n""" \
+        """A log has been written to "%s".\n\nError information:\n""" % \
+        ("https://github.com/Dioptas/Dioptas/issues",
+         "clemens.prescher@gmail.com",
+         os.path.join(os.path.dirname(__file__), log_file))
+    version_info = '\n'.join((separator, "Dioptas Version: %s" % dioptas_version))
+    time_string = time.strftime("%Y-%m-%d, %H:%M:%S")
+    tb_info_file = cStringIO.StringIO()
+    traceback.print_tb(traceback_obj, None, tb_info_file)
+    tb_info_file.seek(0)
+    tb_info = tb_info_file.read()
+    errmsg = '%s: \n%s' % (str(exc_type), str(exc_value))
+    sections = [separator, time_string, separator, errmsg, separator, tb_info]
+    msg = '\n'.join(sections)
+    try:
+        f = open(log_file, "w")
+        f.write(msg)
+        f.write(version_info)
+        f.close()
+    except IOError:
+        pass
+    errorbox = QtGui.QMessageBox()
+    errorbox.setText(str(notice)+str(msg)+str(version_info))
+    errorbox.exec_()
+
+sys.excepthook = excepthook
+
 
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
+    from sys import platform as _platform
+    from controller.MainController import MainController, get_version
+    dioptas_version = get_version()
+    print("Dioptas {}".format(dioptas_version))
+
     if _platform == "linux" or _platform == "linux2" or _platform == "win32" or _platform == 'cygwin':
         app.setStyle('plastique')
-        # possible values:
-        # "windows", "motif", "cde", "plastique", "windowsxp", or "macintosh"
 
     controller = MainController()
     controller.show_window()
