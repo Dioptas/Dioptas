65be361c50a768e9318b7eeb2e8698852a3c748f
Clemens Prescher <clemens.prescher@gmail.com>
2016-06-30 14:38:55 +0200

    finished first implementation of ImgConfigruationManager

--- dioptas/model/ImgConfiguration.py
+++ dioptas/model/ImgConfiguration.py
@@ -5,19 +5,18 @@ from PyQt4 import QtCore
 from . import ImgModel, CalibrationModel, MaskModel
 
 
-class ImgConfiguration(object):
+class ImgConfiguration(QtCore.QObject):
     def __init__(self):
+        super(ImgConfiguration, self).__init__()
         self.img_model = ImgModel()
-        self.mask_model = MaskModel(self.img_model)
-        self.calibration_model = CalibrationModel(self.img_model)
+        self.mask_model = MaskModel()
+        self.calibration_model = CalibrationModel()
 
 
 class ImgConfigurationManager(QtCore.QObject):
-    configuration_added = QtCore.pyqtBoundSignal()
-    configuration_selected = QtCore.pyqtBoundSignal(int)  # new index
-    configuration_removed = QtCore.pyqtBoundSignal()
-
-    img_changed = QtCore.pyqtBoundSignal()
+    configuration_added = QtCore.pyqtSignal()
+    configuration_selected = QtCore.pyqtSignal(int)  # new index
+    configuration_removed = QtCore.pyqtSignal()
 
     def __init__(self):
         super(ImgConfigurationManager, self).__init__()
@@ -25,24 +24,39 @@ class ImgConfigurationManager(QtCore.QObject):
         self.current_configuration = 0
         self.configurations.append(ImgConfiguration())
 
-        self.configurations[self.current_configuration].img_changed.connect(self.img_changed)
-
     def add_configuration(self):
         self.configurations.append(ImgConfiguration())
-        self.configurations[self.current_configuration].img_changed.disconnect(self.img_changed)
         self.current_configuration = len(self.configurations) - 1
-        self.configurations[self.current_configuration].img_changed.connect(self.img_changed)
         self.configuration_added.emit()
-        self.img_changed.emit()
+
+    def remove_configuration(self, ind=-1):
+        del self.configurations[ind]
+        if ind == len(self.configurations) or ind == -1:
+            self.current_configuration = len(self.configurations)-1
+        self.configuration_removed.emit(self.current_configuration)
+
+    def select_configuration(self, ind):
+        if ind>=0 and ind<len(self.configurations):
+            self.current_configuration = ind
+            self.configuration_selected.emit(ind)
 
     @property
     def img_model(self):
+        """
+        :rtype: ImgModel
+        """
         return self.configurations[self.current_configuration].img_model
 
     @property
     def mask_model(self):
+        """
+        :rtype: MaskModel
+        """
         return self.configurations[self.current_configuration].mask_model
 
     @property
     def calibration_model(self):
+        """
+        :rtype: CalibrationModel
+        """
         return self.configurations[self.current_configuration].calibration_model
