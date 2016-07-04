a2caf6190761cc3460ac55b9a3fa3d0d4d16c157
Clemens Prescher <clemens.prescher@gmail.com>
2016-07-04 16:56:21 +0200

    Refactoring to OneModel: test_ConfigurationController Working now

--- dioptas/tests/controller_tests/test_ConfigurationController.py
+++ dioptas/tests/controller_tests/test_ConfigurationController.py
@@ -9,7 +9,7 @@ from PyQt4 import QtGui, QtCore
 from PyQt4.QtTest import QTest
 
 from controller.ConfigurationController import ConfigurationController
-from model.ImgConfiguration import ImgConfigurationManager
+from model.DioptasModel import DioptasModel
 from widgets.ConfigurationWidget import ConfigurationWidget
 
 unittest_path = os.path.dirname(__file__)
@@ -30,10 +30,10 @@ class ConfigurationControllerTest(unittest.TestCase):
 
     def setUp(self):
         self.config_widget = ConfigurationWidget()
-        self.config_manager = ImgConfigurationManager()
+        self.model = DioptasModel()
         self.config_controller = ConfigurationController(
             configuration_widget=self.config_widget,
-            configuration_manager=self.config_manager,
+            dioptas_model=self.model,
             controllers=[]
         )
 
@@ -42,12 +42,12 @@ class ConfigurationControllerTest(unittest.TestCase):
 
     def test_adding_configurations(self):
         click_button(self.config_widget.add_configuration_btn)
-        self.assertEqual(len(self.config_manager.configurations), 2)
+        self.assertEqual(len(self.model.configurations), 2)
         self.assertEqual(len(self.config_widget.configuration_btns), 2)
         self.assertTrue(self.config_widget.configuration_btns[1].isChecked())
 
         click_button(self.config_widget.add_configuration_btn)
-        self.assertEqual(len(self.config_manager.configurations), 3)
+        self.assertEqual(len(self.model.configurations), 3)
         self.assertEqual(len(self.config_widget.configuration_btns), 3)
         self.assertTrue(self.config_widget.configuration_btns[2].isChecked())
 
@@ -60,23 +60,23 @@ class ConfigurationControllerTest(unittest.TestCase):
         click_button(self.config_widget.add_configuration_btn)
         click_button(self.config_widget.add_configuration_btn)
 
-        self.assertEqual(self.config_manager.current_configuration, 3)
+        self.assertEqual(self.model.configuration_ind, 3)
         self.assertTrue(self.config_widget.configuration_btns[-1].isChecked())
 
         click_button(self.config_widget.configuration_btns[0])
-        self.assertEqual(self.config_manager.current_configuration, 0)
+        self.assertEqual(self.model.configuration_ind, 0)
 
     def test_remove_last_configuration(self):
         click_button(self.config_widget.add_configuration_btn)
         click_button(self.config_widget.add_configuration_btn)
         click_button(self.config_widget.add_configuration_btn)
 
-        self.assertEqual(self.config_manager.current_configuration, 3)
+        self.assertEqual(self.model.configuration_ind, 3)
         self.assertTrue(self.config_widget.configuration_btns[3].isChecked())
 
         click_button(self.config_widget.remove_configuration_btn)
 
-        self.assertEqual(self.config_manager.current_configuration, 2)
+        self.assertEqual(self.model.configuration_ind, 2)
         self.assertTrue(self.config_widget.configuration_btns[2].isChecked())
 
     def test_remove_first_configuration(self):
@@ -85,11 +85,13 @@ class ConfigurationControllerTest(unittest.TestCase):
         click_button(self.config_widget.add_configuration_btn)
 
         click_button(self.config_widget.configuration_btns[0])
+        self.assertEqual(self.model.configuration_ind, 0)
+
         click_button(self.config_widget.remove_configuration_btn)
 
-        self.assertEqual(self.config_manager.current_configuration, 0)
+        self.assertEqual(self.model.configuration_ind, 0)
         self.assertTrue(self.config_widget.configuration_btns[0].isChecked())
-        self.assertEqual(len(self.config_manager.configurations), 3)
+        self.assertEqual(len(self.model.configurations), 3)
 
         self.assertEqual(self.config_widget.configuration_btns[0].text(), "1")
         self.assertEqual(self.config_widget.configuration_btns[1].text(), "2")
@@ -103,61 +105,10 @@ class ConfigurationControllerTest(unittest.TestCase):
         click_button(self.config_widget.configuration_btns[1])
         click_button(self.config_widget.remove_configuration_btn)
 
-        self.assertEqual(self.config_manager.current_configuration, 1)
+        self.assertEqual(self.model.configuration_ind, 1)
         self.assertTrue(self.config_widget.configuration_btns[1].isChecked())
-        self.assertEqual(len(self.config_manager.configurations), 3)
+        self.assertEqual(len(self.model.configurations), 3)
 
         self.assertEqual(self.config_widget.configuration_btns[0].text(), "1")
         self.assertEqual(self.config_widget.configuration_btns[1].text(), "2")
         self.assertEqual(self.config_widget.configuration_btns[2].text(), "3")
-
-    def test_changing_models_for_controller(self):
-        controller1 = DummyController()
-        controller2 = DummyController()
-
-        self.config_controller = ConfigurationController(
-            configuration_widget=self.config_widget,
-            configuration_manager=self.config_manager,
-            controllers=[controller1, controller2]
-        )
-
-        click_button(self.config_widget.add_configuration_btn)
-
-        self.assertIs(controller1.img_model, self.config_manager.img_model)
-        self.assertIs(controller2.img_model, self.config_manager.img_model)
-
-        self.assertIs(controller1.mask_model, self.config_manager.mask_model)
-        self.assertIs(controller2.mask_model, self.config_manager.mask_model)
-
-        self.assertIs(controller1.calibration_model, self.config_manager.calibration_model)
-        self.assertIs(controller2.calibration_model, self.config_manager.calibration_model)
-
-        click_button(self.config_widget.configuration_btns[0])
-
-        self.assertIs(controller1.img_model, self.config_manager.img_model)
-        self.assertIs(controller2.img_model, self.config_manager.img_model)
-
-        self.assertIs(controller1.mask_model, self.config_manager.mask_model)
-        self.assertIs(controller2.mask_model, self.config_manager.mask_model)
-
-        self.assertIs(controller1.calibration_model, self.config_manager.calibration_model)
-        self.assertIs(controller2.calibration_model, self.config_manager.calibration_model)
-
-        click_button(self.config_widget.remove_configuration_btn)
-
-        self.assertIs(controller1.img_model, self.config_manager.img_model)
-        self.assertIs(controller2.img_model, self.config_manager.img_model)
-
-        self.assertIs(controller1.mask_model, self.config_manager.mask_model)
-        self.assertIs(controller2.mask_model, self.config_manager.mask_model)
-
-        self.assertIs(controller1.calibration_model, self.config_manager.calibration_model)
-        self.assertIs(controller2.calibration_model, self.config_manager.calibration_model)
-
-
-class DummyController(object):
-    def __init__(self):
-        super(DummyController, self).__init__()
-        self.img_model = None
-        self.mask_model = None
-        self.calibration_model = None
