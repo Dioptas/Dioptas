59e461245f0c456c9f7c1c9336c3efe55ef31c67
Clemens Prescher <clemens.prescher@gmail.com>
2016-07-05 10:57:02 +0200

    All test are passing

--- dioptas/tests/widget_tests/test_ConfigurationWidget.py
+++ dioptas/tests/widget_tests/test_ConfigurationWidget.py
@@ -8,7 +8,7 @@ from mock import MagicMock
 from PyQt4 import QtGui, QtCore
 from PyQt4.QtTest import QTest
 
-from model.ImgConfiguration import ImgConfigurationManager
+from model.DioptasModel import DioptasModel
 from widgets.ConfigurationWidget import ConfigurationWidget
 
 unittest_path = os.path.dirname(__file__)
@@ -29,17 +29,17 @@ class ConfigurationWidgetTest(unittest.TestCase):
 
     def setUp(self):
         self.config_widget = ConfigurationWidget()
-        self.config_manager = ImgConfigurationManager()
+        self.model = DioptasModel()
 
     def test_one_configuration(self):
-        self.config_widget.update_configurations(self.config_manager.configurations, 0)
+        self.config_widget.update_configurations(self.model.configurations, 0)
         self.assertEqual(len(self.config_widget.configuration_btns), 1)
 
     def test_multiple_configurations(self):
-        self.config_manager.add_configuration()
-        self.config_manager.add_configuration()
-        self.config_manager.add_configuration()
-        self.config_widget.update_configurations(self.config_manager.configurations, 1)
+        self.model.add_configuration()
+        self.model.add_configuration()
+        self.model.add_configuration()
+        self.config_widget.update_configurations(self.model.configurations, 1)
 
         self.assertEqual(len(self.config_widget.configuration_btns), 4)
         self.assertFalse(self.config_widget.configuration_btns[0].isChecked())
@@ -49,13 +49,13 @@ class ConfigurationWidgetTest(unittest.TestCase):
 
     def test_configuration_selected_signal(self):
         self.config_widget.configuration_selected = MagicMock()
-        self.config_manager.add_configuration()
-        self.config_manager.add_configuration()
-        self.config_manager.add_configuration()
-        self.config_widget.update_configurations(self.config_manager.configurations, 0)
+        self.model.add_configuration()
+        self.model.add_configuration()
+        self.model.add_configuration()
+        self.config_widget.update_configurations(self.model.configurations, 0)
 
         click_button(self.config_widget.configuration_btns[3])
-        self.config_widget.configuration_selected.assert_called_once_with(3)
+        self.config_widget.configuration_selected.emit.assert_called_once_with(3, True)
 
         self.assertFalse(self.config_widget.configuration_btns[0].isChecked())
         self.assertFalse(self.config_widget.configuration_btns[1].isChecked())
