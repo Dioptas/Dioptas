0ee6c5d803fbaff54d02ae0a6f886f45df923cf8
Clemens Prescher <clemens.prescher@gmail.com>
2016-07-24 13:28:17 +0200

    fixed bug when saving patterns without background subtraction

--- dioptas/tests/controller_tests/test_PatternController.py
+++ dioptas/tests/controller_tests/test_PatternController.py
@@ -3,8 +3,9 @@
 import os
 
 from tests.utility import QtTest
+from mock import MagicMock
 
-from PyQt4 import QtCore
+from PyQt4 import QtCore, QtGui
 from PyQt4.QtTest import QTest
 
 from widgets.integration import IntegrationWidget
@@ -23,9 +24,9 @@ def click_checkbox(checkbox_widget):
                      pos=QtCore.QPoint(2, checkbox_widget.height() / 2.0))
 
 
-class ImageControllerTest(QtTest):
+class PatternControllerTest(QtTest):
     def setUp(self):
-        self.working_dir = {'image': ''}
+        self.working_dir = {'image': '', 'spectrum': ''}
 
         self.widget = IntegrationWidget()
         self.model = DioptasModel()
@@ -34,6 +35,10 @@ class ImageControllerTest(QtTest):
             working_dir=self.working_dir,
             widget=self.widget,
             dioptas_model=self.model)
+
+    def tearDown(self):
+        if os.path.exists(os.path.join(unittest_data_path, "test.xy")):
+            os.remove(os.path.join(unittest_data_path, "test.xy"))
 
     def test_configuration_selected_changes_active_unit_btn(self):
         self.model.add_configuration()
@@ -59,3 +64,10 @@ class ImageControllerTest(QtTest):
         self.assertTrue(self.widget.spec_d_btn.isChecked())
         self.assertEqual(self.widget.pattern_widget.spectrum_plot.getAxis('bottom').labelString(),
                          u"<span style='color: #ffffff'>d (A)</span>")
+
+    def test_save_pattern_without_background(self):
+        QtGui.QFileDialog.getSaveFileName = MagicMock(return_value=os.path.join(unittest_data_path, "test.xy"))
+        self.model.calibration_model.create_file_header = MagicMock(return_value="None")
+        click_button(self.widget.qa_save_spectrum_btn)
+
+        self.assertTrue(os.path.exists(os.path.join(unittest_data_path, "test.xy")))
