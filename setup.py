bc864b9addfcb00292195c0e37ecae691864ae1d
Clemens Prescher <clemens.prescher@gmail.com>
2016-08-23 11:35:23 +0200

    setup.py includes now also smooth_bruckner compiling

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 # -*- coding: utf8 -*-
-
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from numpy.distutils.core import Extension, setup
 
 import versioneer
 
+smooth_bruckner = Extension(
+    name='dioptas.model.util.smooth_bruckner',
+    sources= ['dioptas/model/util/smooth_bruckner.f95']
+)
+
 setup(
-    name='glassure',
+    name='dioptas',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     license='GPLv3',
     author='Clemens Prescher',
     author_email="clemens.prescher@gmail.com",
     url='https://github.com/Dioptas/Dioptas/',
-    install_requires=['numpy', 'scipy', 'lmfit', 'pyqtgraph', 'future', 'pycifrw', 'fabio', 'pyfai'],
-    test_requires=['mock'],
+    install_requires=['numpy', 'scipy', 'lmfit', 'pyqtgraph', 'future', 'pycifrw', 'fabio', 'pyfai', 'scikit-image'],
     description='GUI program for reduction and exploration of 2D X-ray diffraction data',
     classifiers=['Intended Audience :: Science/Research',
                  'Operating System :: OS Independent',
@@ -23,5 +27,8 @@ setup(
     packages=find_packages(),
     package_data={'dioptas': ['calibrants/*',
                               'widgets/stylesheet.qss',
-                              'model/util/data/*.json']}
+                              'model/util/data/*.json',
+                              'model/util/smooth_bruckner.f95']},
+    scripts=['scripts/dioptas'],
+    ext_modules=[smooth_bruckner]
 )
