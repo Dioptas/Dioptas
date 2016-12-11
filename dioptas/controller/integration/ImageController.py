4621e639955230b387063ae34f343925b87ed812
Clemens Prescher <clemens.prescher@gmail.com>
2016-12-11 16:40:21 +0100

    Bugfix: Image is now correctly displayed when using supersampling.

--- dioptas/controller/integration/ImageController.py
+++ dioptas/controller/integration/ImageController.py
@@ -82,7 +82,7 @@ class ImageController(object):
         if auto_scale is None:
             auto_scale = self.widget.img_autoscale_btn.isChecked()
 
-        self.widget.img_widget.plot_image(self.model.img_data,
+        self.widget.img_widget.plot_image(self.model.img_model.get_raw_img_data(),
                                           False)
 
         if auto_scale:
