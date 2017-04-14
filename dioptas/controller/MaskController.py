54f28398c5665bd0ee5853760d1e73f9d310b435
Eran Greenberg <erangre@gmail.com>
2017-04-14 11:49:43 -0500

    Fixed error in case two arc points are placed in the same location. It will now cancel the arc.

--- dioptas/controller/MaskController.py
+++ dioptas/controller/MaskController.py
@@ -252,13 +252,24 @@ class MaskController(object):
         elif self.clicks == 2:
             self.widget.img_widget.mouse_moved.disconnect(self.arc.set_size)
             self.arc.add_point(x, y)
+            if self.arc.vertices[0].x() == self.arc.vertices[1].x() and self.arc.vertices[0].y() == self.arc.vertices[1].y():
+                self.remove_bad_arc()
+                return
             self.widget.img_widget.mouse_moved.connect(self.arc_calc_and_preview)
         elif self.clicks == 3:
             self.arc.add_point(x, y)
             self.widget.img_widget.mouse_moved.disconnect(self.arc_calc_and_preview)
+            if (self.arc.vertices[0].x() == self.arc.vertices[2].x() and self.arc.vertices[0].y() == self.arc.vertices[2].y()) or (self.arc.vertices[1].x() == self.arc.vertices[2].x() and self.arc.vertices[1].y() == self.arc.vertices[2].y()):
+                self.remove_bad_arc()
+                return
             self.widget.img_widget.mouse_moved.connect(self.arc_width_preview)
         elif self.clicks == 4:
             self.finish_arc()
+
+    def remove_bad_arc(self):
+        self.clicks = 0
+        self.widget.img_widget.img_view_box.removeItem(self.arc)
+        self.arc = None
 
     def arc_calc_and_preview(self, x, y):
         v = self.arc.vertices
@@ -320,7 +331,7 @@ class MaskController(object):
         self.model.mask_model.mask_QGraphicsPolygonItem(self.arc)
         self.plot_mask()
         self.widget.img_widget.img_view_box.removeItem(self.arc)
-        self.polygon = None
+        self.arc = None
 
     def below_thresh_btn_click(self):
         thresh = np.float64(self.widget.below_thresh_txt.text())
