aff38c0e2e1b598180ad53223976374e6fff23a5
Clemens Prescher <clemens.prescher@gmail.com>
2017-06-23 15:36:48 +0200

    some refactoring and bugfix for the Configurations save and load tests

--- dioptas/tests/functional_tests/test_ConfigurationSaveLoad.py
+++ dioptas/tests/functional_tests/test_ConfigurationSaveLoad.py
@@ -12,10 +12,47 @@ from qtpy import QtWidgets
 from ...controller.MainController import MainController
 from ..utility import QtTest, click_button
 
-
 unittest_path = os.path.dirname(__file__)
 data_path = os.path.join(unittest_path, '../data')
 jcpds_path = os.path.join(data_path, 'jcpds')
+
+# shared settings for save and load tests
+config_file_path = os.path.join(data_path, 'test_save_load.hdf5')
+
+working_directories = {'image': data_path,
+                       'calibration': data_path,
+                       'phase': os.path.join(data_path, 'jcpds'),
+                       'overlay': data_path,
+                       'mask': data_path,
+                       'pattern': data_path}
+
+integration_unit = 'q_A^-1'
+use_mask = True
+transparent_mask = True
+roi = (100, 120, 300, 400)
+auto_save_integrated_patterns = True
+integrated_patterns_file_formats = ['.xy', '.chi']
+img_autoprocess = True
+detector_thickness = 30
+absorption_length = 175
+test_image_file_name = os.path.join(data_path, 'CeO2_Pilatus1M.tif')
+test_image_file_name_2 = os.path.join(data_path, 'a_CeO2_Pilatus1M.tif')
+test_calibration_file = os.path.join(data_path, 'CeO2_Pilatus1M.poni')
+test_calibration_file_2 = os.path.join(data_path, 'a_CeO2_Pilatus1M.poni')
+poly_order = 55
+pyfai_params = {'detector': 'Detector',
+                'dist': 0.196711580484,
+                'poni1': 0.0813975852141,
+                'poni2': 0.0820662115429,
+                'rot1': 0.00615439716514,
+                'rot2': -0.00156720465515,
+                'rot3': 1.68707221612e-06,
+                'pixel1': 7.9e-05,
+                'pixel2': 7.9e-05,
+                'wavelength': 3.1e-11,
+                'polarization_factor': 0.99,
+                'splineFile': None}
+pressure = 12.0
 
 
 class ConfigurationSaveLoadTest(QtTest):
@@ -54,93 +91,6 @@ class ConfigurationSaveLoadTest(QtTest):
             intermediate_function()
         self.load_configuration()
 
-    def test_save_and_load_configuration_basic(self):
-        self.save_and_load_configuration(None)
-
-    def test_save_and_load_configuration_auto_process(self):
-        self.save_and_load_configuration(self.auto_process_settings)
-        self.assertEqual(self.model.current_configuration.auto_save_integrated_pattern, auto_save_integrated_patterns)
-        self.assertEqual(self.model.current_configuration.integrated_patterns_file_formats,
-                         integrated_patterns_file_formats)
-        self.assertEqual(self.model.current_configuration.img_model.autoprocess, img_autoprocess)
-
-    def auto_process_settings(self):
-        self.model.current_configuration.auto_save_integrated_pattern = auto_save_integrated_patterns
-        self.model.current_configuration.integrated_patterns_file_formats = integrated_patterns_file_formats
-        self.model.current_configuration.img_model.autoprocess = img_autoprocess
-
-    def test_save_and_load_configuration_with_mask(self):
-        self.save_and_load_configuration(self.mask_settings)
-        self.assertEqual(self.model.use_mask, use_mask)
-        self.assertEqual(self.model.transparent_mask, transparent_mask)
-        self.assertTrue(np.array_equal(self.model.mask_model.get_mask(), self.mask_data))
-
-    def mask_settings(self):
-        self.model.current_configuration.use_mask = True
-        self.model.current_configuration.transparent_mask = True
-        self.mask_data = np.eye(self.raw_img_data.shape[0], self.raw_img_data.shape[1], dtype=bool)
-        self.model.mask_model.set_mask(self.mask_data)
-
-    def test_save_and_load_configuration_with_phase(self):
-        self.save_and_load_configuration(self.phase_settings)
-        self.assertEqual(self.model.phase_model.phases[0].params['pressure'], pressure)
-
-    def phase_settings(self):
-        self.load_phase('ar.jcpds')
-        self.model.phase_model.phases[0].params['pressure'] = pressure
-
-    def test_save_and_load_with_overlays(self):
-        self.save_and_load_configuration(self.overlay_settings)
-
-    def overlay_settings(self):
-        self.controller.integration_controller.widget.qa_set_as_overlay_btn.click()
-        self.controller.integration_controller.widget.qa_set_as_overlay_btn.click()
-        self.controller.integration_controller.widget.overlay_set_as_bkg_btn.click()
-        self.current_pattern_x, self.current_pattern_y = \
-            self.model.current_configuration.pattern_model.get_pattern().data
-
-    def test_save_and_load_configuration_with_roi(self):
-        self.save_and_load_configuration(self.roi_settings)
-        self.assertTupleEqual(self.model.current_configuration.roi, roi)
-        self.assertTrue(self.controller.integration_controller.widget.img_roi_btn.isChecked())
-
-    def roi_settings(self):
-        self.controller.integration_controller.image_controller.widget.img_roi_btn.click()
-        self.model.current_configuration.roi = roi
-
-    def cbn_correction_settings(self):
-        self.controller.widget.integration_widget.cbn_groupbox.setChecked(True)
-        self.controller.widget.integration_widget.cbn_diamond_thickness_txt.setText('1.9')
-        self.controller.integration_controller.image_controller.cbn_groupbox_changed()
-
-    def test_save_and_load_configuration_with_oiadac_correction(self):
-        self.save_and_load_configuration(self.oiadac_correction_settings)
-        self.assertEqual(self.model.current_configuration.img_model.img_corrections.
-                         corrections["oiadac"].detector_thickness, 30)
-        self.assertEqual(self.model.current_configuration.img_model.img_corrections.
-                         corrections["oiadac"].absorption_length, 450)
-
-    def oiadac_correction_settings(self):
-        self.controller.widget.integration_widget.oiadac_groupbox.setChecked(True)
-        self.controller.widget.integration_widget.oiadac_thickness_txt.setText('30')
-        self.controller.widget.integration_widget.oiadac_abs_length_txt.setText('450')
-        self.controller.integration_controller.image_controller.oiadac_groupbox_changed()
-
-    def test_save_and_load_configuration_in_cake_mode(self):
-        self.save_and_load_configuration(self.cake_settings)
-        self.assertTrue(self.model.current_configuration.auto_integrate_cake)
-
-    def cake_settings(self):
-        self.controller.integration_controller.widget.img_mode_btn.click()
-
-    def test_save_and_load_with_fit_bg(self):
-        self.save_and_load_configuration(self.fit_bg_settings)
-        self.assertEqual(self.widget.integration_widget.bkg_pattern_poly_order_sb.value(), poly_order)
-
-    def fit_bg_settings(self):
-        self.controller.integration_controller.widget.qa_bkg_pattern_btn.click()
-        self.controller.integration_controller.widget.bkg_pattern_poly_order_sb.setValue(poly_order)
-
     def existing_files_intermediate_settings(self):
         self.check_calibration = False
 
@@ -161,45 +111,107 @@ class ConfigurationSaveLoadTest(QtTest):
             saved_pyfai_params, _ = self.model.calibration_model.get_calibration_parameter()
             self.assertDictEqual(saved_pyfai_params, pyfai_params)
 
+    ####################################################################################################################
+    def test_save_and_load_configuration_basic(self):
+        self.save_and_load_configuration(None)
+
+    def test_with_auto_processing(self):
+        self.save_and_load_configuration(self.auto_process_settings)
+        self.assertEqual(self.model.current_configuration.auto_save_integrated_pattern, auto_save_integrated_patterns)
+        self.assertEqual(self.model.current_configuration.integrated_patterns_file_formats,
+                         integrated_patterns_file_formats)
+        self.assertEqual(self.model.current_configuration.img_model.autoprocess, img_autoprocess)
+
+    def auto_process_settings(self):
+        self.model.current_configuration.auto_save_integrated_pattern = auto_save_integrated_patterns
+        self.model.current_configuration.integrated_patterns_file_formats = integrated_patterns_file_formats
+        self.model.current_configuration.img_model.autoprocess = img_autoprocess
+
+    ####################################################################################################################
+    def test_with_mask(self):
+        self.save_and_load_configuration(self.mask_settings)
+        self.assertEqual(self.model.use_mask, use_mask)
+        self.assertEqual(self.model.transparent_mask, transparent_mask)
+        self.assertTrue(np.array_equal(self.model.mask_model.get_mask(), self.mask_data))
+
+    def mask_settings(self):
+        self.model.current_configuration.use_mask = True
+        self.model.current_configuration.transparent_mask = True
+        self.mask_data = np.eye(self.raw_img_data.shape[0], self.raw_img_data.shape[1], dtype=bool)
+        self.model.mask_model.set_mask(self.mask_data)
+
+    ####################################################################################################################
+    def test_with_phases(self):
+        self.save_and_load_configuration(self.phase_settings)
+        self.assertEqual(self.model.phase_model.phases[0].params['pressure'], pressure)
+
+    def phase_settings(self):
+        self.load_phase('ar.jcpds')
+        self.model.phase_model.phases[0].params['pressure'] = pressure
+
     def load_phase(self, filename):
         QtWidgets.QFileDialog.getOpenFileNames = MagicMock(return_value=[os.path.join(jcpds_path, filename)])
         click_button(self.controller.widget.integration_widget.phase_add_btn)
 
-# shared settings for save and load tests
+    ####################################################################################################################
+    def test_with_overlays(self):
+        self.save_and_load_configuration(self.overlay_settings)
 
-config_file_path = os.path.join(data_path, 'test_save_load.hdf5')
+    def overlay_settings(self):
+        self.controller.integration_controller.widget.qa_set_as_overlay_btn.click()
+        self.controller.integration_controller.widget.qa_set_as_overlay_btn.click()
+        self.controller.integration_controller.widget.overlay_set_as_bkg_btn.click()
+        self.current_pattern_x, self.current_pattern_y = \
+            self.model.current_configuration.pattern_model.get_pattern().data
 
-working_directories = {'image': data_path,
-                       'calibration': data_path,
-                       'phase': os.path.join(data_path, 'jcpds'),
-                       'overlay': data_path,
-                       'mask': data_path,
-                       'pattern': data_path}
+    ####################################################################################################################
+    def test_with_roi(self):
+        self.save_and_load_configuration(self.roi_settings)
+        self.assertTupleEqual(self.model.current_configuration.roi, roi)
+        self.assertTrue(self.controller.integration_controller.widget.img_roi_btn.isChecked())
 
-integration_unit = 'q_A^-1'
-use_mask = True
-transparent_mask = True
-roi = (100, 120, 300, 400)
-auto_save_integrated_patterns = True
-integrated_patterns_file_formats = ['.xy', '.chi']
-img_autoprocess = True
-detector_thickness = 30
-absorption_length = 175
-test_image_file_name = os.path.join(data_path, 'CeO2_Pilatus1M.tif')
-test_image_file_name_2 = os.path.join(data_path, 'a_CeO2_Pilatus1M.tif')
-test_calibration_file = os.path.join(data_path, 'CeO2_Pilatus1M.poni')
-test_calibration_file_2 = os.path.join(data_path, 'a_CeO2_Pilatus1M.poni')
-poly_order = 55
-pyfai_params = {'detector': 'Detector',
-                'dist': 0.196711580484,
-                'poni1': 0.0813975852141,
-                'poni2': 0.0820662115429,
-                'rot1': 0.00615439716514,
-                'rot2': -0.00156720465515,
-                'rot3': 1.68707221612e-06,
-                'pixel1': 7.9e-05,
-                'pixel2': 7.9e-05,
-                'wavelength': 3.1e-11,
-                'polarization_factor': 0.99,
-                'splineFile': None}
-pressure = 12.0
+    def roi_settings(self):
+        self.controller.integration_controller.image_controller.widget.img_roi_btn.click()
+        self.model.current_configuration.roi = roi
+
+    ####################################################################################################################
+    def test_with_cbn_correction(self):
+        self.save_and_load_configuration(self.cbn_correction_settings)
+        self.assertEqual(self.model.current_configuration.img_model.img_corrections.
+                         corrections["cbn"]._diamond_thickness, 1.9)
+
+    def cbn_correction_settings(self):
+        self.controller.widget.integration_widget.cbn_groupbox.setChecked(True)
+        self.controller.widget.integration_widget.cbn_diamond_thickness_txt.setText('1.9')
+        self.controller.integration_controller.image_controller.cbn_groupbox_changed()
+
+    ####################################################################################################################
+    def test_with_oiadac_correction(self):
+        self.save_and_load_configuration(self.oiadac_correction_settings)
+        self.assertEqual(self.model.current_configuration.img_model.img_corrections.
+                         corrections["oiadac"].detector_thickness, 30)
+        self.assertEqual(self.model.current_configuration.img_model.img_corrections.
+                         corrections["oiadac"].absorption_length, 450)
+
+    def oiadac_correction_settings(self):
+        self.controller.widget.integration_widget.oiadac_groupbox.setChecked(True)
+        self.controller.widget.integration_widget.oiadac_thickness_txt.setText('30')
+        self.controller.widget.integration_widget.oiadac_abs_length_txt.setText('450')
+        self.controller.integration_controller.image_controller.oiadac_groupbox_changed()
+
+    ####################################################################################################################
+    def test_save_and_load_configuration_in_cake_mode(self):
+        self.save_and_load_configuration(self.cake_settings)
+        self.assertTrue(self.model.current_configuration.auto_integrate_cake)
+
+    def cake_settings(self):
+        self.controller.integration_controller.widget.img_mode_btn.click()
+
+    ####################################################################################################################
+    def test_with_fit_bg(self):
+        self.save_and_load_configuration(self.fit_bg_settings)
+        self.assertEqual(self.widget.integration_widget.bkg_pattern_poly_order_sb.value(), poly_order)
+
+    def fit_bg_settings(self):
+        self.controller.integration_controller.widget.qa_bkg_pattern_btn.click()
+        self.controller.integration_controller.widget.bkg_pattern_poly_order_sb.setValue(poly_order)
