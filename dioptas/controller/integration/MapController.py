231b96283a26301fee82c6cbd3773532c40864a1
Eran Greenberg <erangre@gmail.com>
2018-12-01 13:36:46 +0200

    Fixed small problem with wrong map info showing up.

--- dioptas/controller/integration/MapController.py
+++ dioptas/controller/integration/MapController.py
@@ -144,10 +144,10 @@ class MapController(object):
         if self.map_model.all_positions_defined_in_files or self.map_model.positions_set_manually:
             map_size = str(self.map_model.num_hor) + 'x' + str(self.map_model.num_ver)
             map_range = '\tX:\tRange: ' + "{:.4f}".format(self.map_model.min_hor) + ',' + \
-                        "{:.4f}".format(self.map_model.min_hor + self.map_model.num_hor*self.map_model.diff_hor) + \
+                        "{:.4f}".format(self.map_model.min_hor + (self.map_model.num_hor-1)*self.map_model.diff_hor) + \
                         '\tStep: ' + "{:.4f}".format(self.map_model.diff_hor) + '\n' + '\tY:\tRange: ' + \
                         "{:.4f}".format(self.map_model.min_ver) + ',' + \
-                        "{:.4f}".format(self.map_model.min_ver + self.map_model.num_ver*self.map_model.diff_ver) + \
+                        "{:.4f}".format(self.map_model.min_ver + (self.map_model.num_ver-1)*self.map_model.diff_ver) + \
                         '\tStep: ' + "{:.4f}".format(self.map_model.diff_ver)
             self.map_widget.map_status_size_and_step_lbl.setText(map_size + map_range)
             self.map_widget.map_status_size_and_step_lbl.setStyleSheet('color: green')
