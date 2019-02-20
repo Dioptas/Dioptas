f55e31408630637d35f3662ea2633f68066d5cc2
Clemens Prescher <clemens.prescher@gmail.com>
2019-02-20 06:08:37 +0100

    Updated the Copyright Information in all files

--- dioptas/widgets/CustomWidgets.py
+++ dioptas/widgets/CustomWidgets.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # Dioptas - GUI program for fast processing of 2D X-ray diffraction data
-# Copyright (C) 2017  Clemens Prescher (clemens.prescher@gmail.com)
-# Institute for Geology and Mineralogy, University of Cologne
+# Principal author: Clemens Prescher (clemens.prescher@gmail.com)
+# Copyright (C) 2013-2019 GSECARS, University of Chicago, USA
+# Copyright (C) 2015-2018 Institute for Geology and Mineralogy, University of Cologne, Germany
+# Copyright (C) 2019 DESY, Hamburg, Germany
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
