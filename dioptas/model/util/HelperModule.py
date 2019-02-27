e2226fc56e77c296781ba9ec12aec4ae1ab8dc5b
Eran Greenberg <erangre@gmail.com>
2019-02-27 10:23:20 -0600

    Fixed FileNameIterator. Now if going from "10" to "9" does not work because it is expecting "09" then it also checks if it exists without the leading zeros.

--- dioptas/model/util/HelperModule.py
+++ dioptas/model/util/HelperModule.py
@@ -90,13 +90,20 @@ class FileNameIterator(QtCore.QObject):
 
         for ind, match in enumerate(reversed(list(match_iterator))):
             number_span = match.span()
+            print(number_span)
             left_ind = number_span[0]
             right_ind = number_span[1]
             number = int(file_str[left_ind:right_ind]) + step
+            print(number)
             new_file_str = "{left_str}{number:0{len}}{right_str}".format(
                 left_str=file_str[:left_ind],
                 number=number,
                 len=right_ind - left_ind,
+                right_str=file_str[right_ind:]
+            )
+            new_file_str_no_leading_zeros = "{left_str}{number}{right_str}".format(
+                left_str=file_str[:left_ind],
+                number=number,
                 right_str=file_str[right_ind:]
             )
             if pos is None:
@@ -104,11 +111,21 @@ class FileNameIterator(QtCore.QObject):
                 if os.path.exists(new_complete_path):
                     self.complete_path = new_complete_path
                     return new_complete_path
+                else:
+                    new_complete_path = os.path.join(directory, new_file_str_no_leading_zeros)
+                    if os.path.exists(new_complete_path):
+                        self.complete_path = new_complete_path
+                        return new_complete_path
             elif ind == pos:
                 new_complete_path = os.path.join(directory, new_file_str)
                 if os.path.exists(new_complete_path):
                     self.complete_path = new_complete_path
                     return new_complete_path
+                else:
+                    new_complete_path = os.path.join(directory, new_file_str_no_leading_zeros)
+                    if os.path.exists(new_complete_path):
+                        self.complete_path = new_complete_path
+                        return new_complete_path
         return None
 
     def _iterate_folder_number(self, path, step, mec_mode=False):
