5b0eab9302d4beb5801ab0eed69028d7e73f910c
Eran Greenberg <erangre@gmail.com>
2019-08-22 14:09:46 -0500

    Histogram was changing upon changing tabs. This was annoying when switching between integration and mask tabs when slowly masking the image.
    Now created a list of old histogram levels, with entries for every tab, and if it is still None it will take from the previous tab, but if a tab has already been used once it will remember the histogram levels it had.

--- dioptas/controller/MainController.py
+++ dioptas/controller/MainController.py
@@ -45,6 +45,7 @@ class MainController(object):
 
         self.use_settings = use_settings
         self.widget = MainWidget()
+        self.old_hist_levels = [None, None, None]
 
         # create data
         if settings_directory == 'default':
@@ -134,18 +135,20 @@ class MainController(object):
 
         # get the old view range
         old_view_range = None
-        old_hist_levels = None
         if old_index == 0:  # calibration tab
             old_view_range = self.widget.calibration_widget.img_widget.img_view_box.targetRange()
             old_hist_levels = self.widget.calibration_widget.img_widget.img_histogram_LUT_horizontal.getExpLevels()
+            self.old_hist_levels[0] = old_hist_levels
         elif old_index == 1:  # mask tab
             old_view_range = self.widget.mask_widget.img_widget.img_view_box.targetRange()
             old_hist_levels = self.widget.mask_widget.img_widget.img_histogram_LUT_horizontal.getExpLevels()
+            self.old_hist_levels[1] = old_hist_levels
         elif old_index == 2:
             old_view_range = self.widget.integration_widget.img_widget.img_view_box.targetRange()
             old_hist_levels = self.widget.integration_widget.img_widget.img_histogram_LUT_horizontal.getExpLevels()
+            self.old_hist_levels[2] = old_hist_levels
 
-        # update the GUI
+            # update the GUI
         if ind == 2:  # integration tab
             self.model.mask_model.set_supersampling()
             self.integration_controller.image_controller.plot_mask()
@@ -162,13 +165,22 @@ class MainController(object):
             else:
                 self.model.pattern_changed.emit()
             self.widget.integration_widget.img_widget.set_range(x_range=old_view_range[0], y_range=old_view_range[1])
-            self.widget.integration_widget.img_widget.img_histogram_LUT_horizontal.setLevels(*old_hist_levels)
-            self.widget.integration_widget.img_widget.img_histogram_LUT_vertical.setLevels(*old_hist_levels)
+            if self.old_hist_levels[2]:
+                self.widget.integration_widget.img_widget.img_histogram_LUT_horizontal.setLevels(
+                    *(self.old_hist_levels[2]))
+                self.widget.integration_widget.img_widget.img_histogram_LUT_vertical.setLevels(
+                    *(self.old_hist_levels[2]))
+            else:
+                self.widget.integration_widget.img_widget.img_histogram_LUT_horizontal.setLevels(*old_hist_levels)
+                self.widget.integration_widget.img_widget.img_histogram_LUT_vertical.setLevels(*old_hist_levels)
         elif ind == 1:  # mask tab
             self.mask_controller.plot_mask()
             self.mask_controller.plot_image()
             self.widget.mask_widget.img_widget.set_range(x_range=old_view_range[0], y_range=old_view_range[1])
-            self.widget.mask_widget.img_widget.img_histogram_LUT_vertical.setLevels(*old_hist_levels)
+            if self.old_hist_levels[1]:
+                self.widget.mask_widget.img_widget.img_histogram_LUT_vertical.setLevels(*(self.old_hist_levels[1]))
+            else:
+                self.widget.mask_widget.img_widget.img_histogram_LUT_vertical.setLevels(*old_hist_levels)
         elif ind == 0:  # calibration tab
             self.calibration_controller.plot_mask()
             try:
@@ -176,7 +188,11 @@ class MainController(object):
             except (TypeError, AttributeError):
                 pass
             self.widget.calibration_widget.img_widget.set_range(x_range=old_view_range[0], y_range=old_view_range[1])
-            self.widget.calibration_widget.img_widget.img_histogram_LUT_vertical.setLevels(*old_hist_levels)
+            if self.old_hist_levels[0]:
+                self.widget.calibration_widget.img_widget.img_histogram_LUT_vertical.setLevels(
+                    *(self.old_hist_levels[0]))
+            else:
+                self.widget.calibration_widget.img_widget.img_histogram_LUT_vertical.setLevels(*old_hist_levels)
 
     def update_title(self):
         """
