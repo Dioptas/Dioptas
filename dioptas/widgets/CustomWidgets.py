55ef401fd874e4c264eeeffb7c16e24e5b54f2b1
Clemens Prescher <clemens.prescher@gmail.com>
2020-04-22 18:28:36 +0200

    Updated year to 2020

--- dioptas/widgets/CustomWidgets.py
+++ dioptas/widgets/CustomWidgets.py
@@ -3,7 +3,7 @@
 # Principal author: Clemens Prescher (clemens.prescher@gmail.com)
 # Copyright (C) 2013-2019 GSECARS, University of Chicago, USA
 # Copyright (C) 2015-2018 Institute for Geology and Mineralogy, University of Cologne, Germany
-# Copyright (C) 2019 DESY, Hamburg, Germany
+# Copyright (C) 2019-2020 DESY, Hamburg, Germany
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
