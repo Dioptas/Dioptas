e9fc7061fa60bb797b0f945ed13f4143ab31fc6f
karnem <mikhail.karnevskiy@desy.de>
2021-09-13 12:41:41 +0200

    Add timestamp to new dioptas file.

--- scripts/dropbox_upload.py
+++ scripts/dropbox_upload.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import os
+from datetime import datetime
 
 from dropbox import Dropbox
 
@@ -12,6 +13,13 @@ if os.getenv('DROPBOX_TOKEN') == None:
     print('Please set the Dropbox api Access token as environment variable DROPBOX_TOKEN')
     exit(-1)
 
+target_path = sys.argv[2]
+dir_name, file_name = os.path.split(target_path)
+print("Name: ", target_path, file_name)
+base_name = "_".join(file_name.split("_")[1:])
+print("base ", dir_name, base_name)
+ts = datetime.now().strftime("%Y-%m-%d_%H-%M")
+new_path = f"{dir_name}/Dioptas_{ts}_{base_name}"
 db = Dropbox(os.getenv('DROPBOX_TOKEN'))
-db.files_upload(open(sys.argv[1], 'rb').read(), sys.argv[2])
-print('{} successfully uploaded to dropbox folder {}'.format(sys.argv[1], sys.argv[2]))
+db.files_upload(open(sys.argv[1], 'rb').read(), new_path)
+print('{} successfully uploaded to dropbox folder {}'.format(sys.argv[1], new_path))
