062b092fb20b33b84dc272c72d9ea1c49d820474
Clemens Prescher <clemens.prescher@gmail.com>
2021-10-10 14:44:10 +0200

    Revert changes to dropbox upload script

--- scripts/dropbox_upload.py
+++ scripts/dropbox_upload.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import sys
 import os
-from datetime import datetime
 
 from dropbox import Dropbox
 
@@ -13,11 +12,6 @@ if os.getenv('DROPBOX_TOKEN') == None:
     print('Please set the Dropbox api Access token as environment variable DROPBOX_TOKEN')
     exit(-1)
 
-target_path = sys.argv[2]
-dir_name, file_name = os.path.split(target_path)
-base_name = "_".join(file_name.split("_")[1:])
-ts = datetime.now().strftime("%Y-%m-%d_%H-%M")
-new_path = f"{dir_name}/Dioptas_{ts}_{base_name}"
 db = Dropbox(os.getenv('DROPBOX_TOKEN'))
-db.files_upload(open(sys.argv[1], 'rb').read(), new_path)
-print('{} successfully uploaded to dropbox folder {}'.format(sys.argv[1], new_path))
+db.files_upload(open(sys.argv[1], 'rb').read(), sys.argv[2])
+print('{} successfully uploaded to dropbox folder {}'.format(sys.argv[1], sys.argv[2]))
