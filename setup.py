b74ea472a3b99447cb1c7d8b2f641e5c9c2784f1
Clemens Prescher <clemens.prescher@gmail.com>
2021-10-10 14:11:49 +0200

    pip install working now correctly

--- setup.py
+++ setup.py
@@ -1,33 +1,27 @@
 # -*- coding: utf-8 -*-
-from setuptools import find_packages
-from numpy.distutils.core import Extension, setup
-
+from setuptools import find_packages, setup
+from Cython.Build import cythonize
 import versioneer
 
-with_bruckner_cython = True
-with_bruckner_f95 = False
-
 ext_modules = []
-if with_bruckner_cython:
-    from Cython.Build import cythonize
+ext_modules = cythonize('dioptas/model/util/smooth_bruckner_cython.pyx')
 
-    ext_modules = cythonize('dioptas/model/util/smooth_bruckner_cython.pyx')
+version = versioneer.get_version()
 
-if with_bruckner_f95:
-    ext_modules.append(Extension(
-        name='dioptas.model.util.smooth_bruckner',
-        sources=['dioptas/model/util/smooth_bruckner.f95'])
-    )
+if version=="0+unknown":
+    version="0.5.3"
 
 setup(
     name='dioptas',
-    version=versioneer.get_version(),
+    version=version,
     cmdclass=versioneer.get_cmdclass(),
     license='GPLv3',
     author='Clemens Prescher',
     author_email="clemens.prescher@gmail.com",
     url='https://github.com/Dioptas/Dioptas/',
-    install_requires=['numpy', 'cython'],
+    install_requires=['cython', 'extra_data', 'future', 'h5py', 'hdf5plugin', 'lmfit', 'pandas', 'pycifrw',
+                      'python-dateutil', 'pyinstaller', 'pyqt5', 'pyfai', 'pyepics', 'pyopengl', 'pyopengl-accelerate',
+                      'pyqtgraph', 'qtpy', 'scikit-image', 'sharedmem', 'watchdog', ],
     description='GUI program for reduction and exploration of 2D X-ray diffraction data',
     classifiers=['Intended Audience :: Science/Research',
                  'Operating System :: OS Independent',
@@ -39,6 +33,7 @@ setup(
                               'resources/calibrants/*',
                               'resources/data/*',
                               'resources/icons/*',
+                              '__version__'
                               ]
                   },
     scripts=['scripts/dioptas', 'scripts/dioptas_batch'],
