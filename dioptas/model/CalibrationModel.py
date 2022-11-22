Fix Issue overflow error during detector rotation

dioptas/model/CalibrationModel.py
@@ -847,7 +847,8 @@ class CalibrationModel(object):
         """
         :param transform_function: function pointer which will affect the dx, dy and pixel corners of the detector
         """
-        self.detector._pixel_corners = np.ascontiguousarray(transform_function(self.detector.get_pixel_corners()))
+        if self.detector._pixel_corners is not None:
+            self.detector._pixel_corners = np.ascontiguousarray(transform_function(self.detector.get_pixel_corners()))
 
     def _swap_pixel_size(self):
         """swaps the pixel sizes"""