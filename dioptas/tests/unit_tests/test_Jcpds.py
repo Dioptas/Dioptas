b5d266099746c16957a45675fc8bc53ace3d1772
Clemens Prescher <clemens.prescher@gmail.com>
2023-04-22 17:05:08 +0200

    Refactoring of remaining unittests into pytest function style tests

--- dioptas/tests/unit_tests/test_Jcpds.py
+++ dioptas/tests/unit_tests/test_Jcpds.py
@@ -17,102 +17,101 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import unittest
-import gc
 import os
-from ...model.util import jcpds
+
+import pytest
+
+from ...model.util import jcpds as jcpds_class
 
 unittest_path = os.path.dirname(__file__)
 data_path = os.path.join(unittest_path, '../data')
 jcpds_path = os.path.join(data_path, 'jcpds')
 
 
-class JcpdsUnitTest(unittest.TestCase):
-    def setUp(self):
-        self.jcpds = jcpds()
-
-    def tearDown(self):
-        del self.jcpds
-        gc.collect()
-
-    def test_sorting_of_reflections(self):
-        self.jcpds.add_reflection(1, 0, 0, 100, 4.0)
-        self.jcpds.add_reflection(1, 2, 0, 90, 2.0)
-        self.jcpds.add_reflection(2, 2, 0, 23, 3.0)
-        self.jcpds.add_reflection(5, 2, 1, 50, 6.0)
-        self.jcpds.add_reflection(3, 2, 2, 10, 41.0)
-        self.jcpds.add_reflection(4, 3, 0, 30, 1.0)
-        self.jcpds.add_reflection(2, 2, 5, 2, 0.3)
-
-        self.jcpds.sort_reflections_by_h()
-        self.assertEqual(self.jcpds.reflections[0].d0, 4.0)
-        self.assertEqual(self.jcpds.reflections[6].d0, 6.0)
-
-        self.jcpds.sort_reflections_by_k()
-        self.assertEqual(self.jcpds.reflections[0].d0, 4.0)
-        self.assertEqual(self.jcpds.reflections[6].d0, 1.0)
-
-        self.jcpds.sort_reflections_by_l()
-        self.assertEqual(self.jcpds.reflections[0].d0, 4.0)
-        self.assertEqual(self.jcpds.reflections[6].d0, 0.3)
-
-        self.jcpds.sort_reflections_by_intensity()
-        self.assertEqual(self.jcpds.reflections[0].d0, 0.3)
-        self.assertEqual(self.jcpds.reflections[6].d0, 4.0)
-
-        self.jcpds.sort_reflections_by_d()
-        self.assertEqual(self.jcpds.reflections[0].intensity, 2)
-        self.assertEqual(self.jcpds.reflections[6].intensity, 10)
-
-    def test_modified_flag(self):
-        self.assertFalse(self.jcpds.params['modified'])
-        self.jcpds.params['a0'] = 3
-        self.assertTrue(self.jcpds.params['modified'])
-        self.assertEqual(self.jcpds.params['a0'], 3)
-        self.jcpds.modified = False
-
-        self.jcpds.load_file(os.path.join(jcpds_path, 'au_Anderson.jcpds'))
-        self.assertFalse(self.jcpds.params['modified'])
-        self.jcpds.params['k0'] = 200
-        self.assertTrue(self.jcpds.params['modified'])
-        self.assertEqual(os.path.join(jcpds_path, 'au_Anderson.jcpds*'), self.jcpds.filename)
-        self.assertEqual('au_Anderson*', self.jcpds.name)
-
-    def get_reflection_d_spacing(self, reflections, h, k, l):
-        for reflection in reflections:
-            if reflection.h == h and reflection.k == k and reflection.l == l:
-                return reflection.d0
-
-    def test_consistency_d_spacing_calculation(self):
-        # loading a monoclinic jcpds and check if different signs will change the d spacing
-        self.jcpds.load_file(os.path.join(jcpds_path, 'FeGeO3_cpx.jcpds'))
-
-        d1_mon = self.get_reflection_d_spacing(self.jcpds.reflections, 2, 2, 1)
-        d2_mon = self.get_reflection_d_spacing(self.jcpds.reflections, -2, 2, 1)
-
-        self.jcpds.params['symmetry'] = 'TRICLINIC'
-        self.jcpds.compute_d0()
-
-        d1_tri = self.get_reflection_d_spacing(self.jcpds.reflections, 2, 2, 1)
-        d2_tri = self.get_reflection_d_spacing(self.jcpds.reflections, -2, 2, 1)
-
-        self.assertAlmostEqual(d1_mon, d1_tri)
-        self.assertAlmostEqual(d2_mon, d2_tri)
-
-    def test_using_negative_pressures(self):
-        self.jcpds.load_file(os.path.join(jcpds_path, 'au_Anderson.jcpds'))
-        self.jcpds.pressure = -1.
-
-        self.jcpds.compute_d(-1, 298)
-        self.assertGreater(self.jcpds.params['v'], self.jcpds.params['v0'])
-
-    def test_using_negative_pressures_with_zero_bulk_modulus(self):
-        self.jcpds.load_file(os.path.join(jcpds_path, 're_K0.jcpds'))
-        self.jcpds.pressure = -1.
-
-        self.jcpds.compute_d(-1, 298)
-        self.assertEqual(self.jcpds.params['v'], self.jcpds.params['v0'])
+@pytest.fixture
+def jcpds():
+    return jcpds_class()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_sorting_of_reflections(jcpds):
+    jcpds.add_reflection(1, 0, 0, 100, 4.0)
+    jcpds.add_reflection(1, 2, 0, 90, 2.0)
+    jcpds.add_reflection(2, 2, 0, 23, 3.0)
+    jcpds.add_reflection(5, 2, 1, 50, 6.0)
+    jcpds.add_reflection(3, 2, 2, 10, 41.0)
+    jcpds.add_reflection(4, 3, 0, 30, 1.0)
+    jcpds.add_reflection(2, 2, 5, 2, 0.3)
+
+    jcpds.sort_reflections_by_h()
+    assert jcpds.reflections[0].d0 == 4.0
+    assert jcpds.reflections[6].d0 == 6.0
+
+    jcpds.sort_reflections_by_k()
+    assert jcpds.reflections[0].d0 == 4.0
+    assert jcpds.reflections[6].d0 == 1.0
+
+    jcpds.sort_reflections_by_l()
+    assert jcpds.reflections[0].d0 == 4.0
+    assert jcpds.reflections[6].d0 == 0.3
+
+    jcpds.sort_reflections_by_intensity()
+    assert jcpds.reflections[0].d0 == 0.3
+    assert jcpds.reflections[6].d0 == 4.0
+
+    jcpds.sort_reflections_by_d()
+    assert jcpds.reflections[0].intensity == 2
+    assert jcpds.reflections[6].intensity == 10
+
+
+def test_modified_flag(jcpds):
+    assert not jcpds.params['modified']
+    jcpds.params['a0'] = 3
+    assert jcpds.params['modified']
+    assert jcpds.params['a0'] == 3
+    jcpds.modified = False
+
+    jcpds.load_file(os.path.join(jcpds_path, 'au_Anderson.jcpds'))
+    assert not jcpds.params['modified']
+    jcpds.params['k0'] = 200
+    assert jcpds.params['modified']
+    assert os.path.join(jcpds_path, 'au_Anderson.jcpds*') == jcpds.filename
+    assert 'au_Anderson*' == jcpds.name
+
+
+def get_reflection_d_spacing(reflections, h, k, l):
+    for reflection in reflections:
+        if reflection.h == h and reflection.k == k and reflection.l == l:
+            return reflection.d0
+
+
+def test_consistency_d_spacing_calculation(jcpds):
+    # loading a monoclinic jcpds and check if different signs will change the d spacing
+    jcpds.load_file(os.path.join(jcpds_path, 'FeGeO3_cpx.jcpds'))
+
+    d1_mon = get_reflection_d_spacing(jcpds.reflections, 2, 2, 1)
+    d2_mon = get_reflection_d_spacing(jcpds.reflections, -2, 2, 1)
+
+    jcpds.params['symmetry'] = 'TRICLINIC'
+    jcpds.compute_d0()
+
+    d1_tri = get_reflection_d_spacing(jcpds.reflections, 2, 2, 1)
+    d2_tri = get_reflection_d_spacing(jcpds.reflections, -2, 2, 1)
+
+    assert d1_mon == pytest.approx(d1_tri)
+    assert d2_mon == pytest.approx(d2_tri)
+
+
+def test_using_negative_pressures(jcpds):
+    jcpds.load_file(os.path.join(jcpds_path, 'au_Anderson.jcpds'))
+    jcpds.pressure = -1.
+
+    jcpds.compute_d(-1, 298)
+    assert jcpds.params['v'] > jcpds.params['v0']
+
+
+def test_using_negative_pressures_with_zero_bulk_modulus(jcpds):
+    jcpds.load_file(os.path.join(jcpds_path, 're_K0.jcpds'))
+    jcpds.pressure = -1.
+
+    jcpds.compute_d(-1, 298)
+    assert jcpds.params['v'] == jcpds.params['v0']
