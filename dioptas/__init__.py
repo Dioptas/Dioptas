552694e6535f70b9f8a4cddc699cf0bd41890372
Thomas VINCENT <thomas.vincent@esrf.fr>
2023-09-08 14:43:26 +0200

    request pyqt6 by default

--- dioptas/__init__.py
+++ dioptas/__init__.py
@@ -20,8 +20,17 @@
 
 from __future__ import absolute_import
 
+import os
 import sys
 from sys import platform as _platform
+
+# If QT_API is not set, use PyQt6 by default
+if "QT_API" not in os.environ:
+    try:
+        import PyQt6.QtCore
+    except ImportError:
+        pass
+
 from qtpy import QtWidgets
 
 __version__ = "0.5.8-dev-alpha"
