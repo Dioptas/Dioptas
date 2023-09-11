337c1231aee103f54b5b0ff191e73f8294defb87
Clemens Prescher <clemens.prescher@gmail.com>
2023-09-11 16:40:53 +0200

    Created another approach for the Batchmodel(2)

---
+++ dioptas/model/MapModel2.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+# Dioptas - GUI program for fast processing of 2D X-ray diffraction data
+# Principal author: Clemens Prescher (clemens.prescher@gmail.com)
+# Copyright (C) 2014-2019 GSECARS, University of Chicago, USA
+# Copyright (C) 2015-2018 Institute for Geology and Mineralogy, University of Cologne, Germany
+# Copyright (C) 2019-2020 DESY, Hamburg, Germany
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import os.path
+
+import numpy as np
+from qtpy import QtCore
+
+from .Configuration import Configuration
+
+
+class MapPointInfo:
+    filename: str
+    frame_index: int
+
+    def __init__(self, filename, frame_index):
+        self.filename = filename
+        self.frame_index = frame_index
+
+
+class MapModel2(QtCore.QObject):
+    map_changed = QtCore.Signal()
+
+    def __init__(self, configuration: Configuration):
+        """
+        Creates a new map-model. The configuration specified will serve as
+        integrator for the processed files.
+        :param configuration: Configuration to be used for integration
+        """
+        super().__init__()
+        self.configuration = configuration
+        self.filenames = None
+        self.point_infos = []
+        self.pattern_intensities = None
+        self.pattern_x = None
+        self.window = None
+        self.window_intensities = None
+        self.dimension = None
+        self.possible_dimensions = None
+        self.map = None
+
+    def create_map(self, filenames: list[str]):
+        self.filenames = filenames
+        self.integrate()
+
+        if self.window is None:
+            self.window = get_center_window(self.pattern_x)
+
+        self.window_intensities = get_window_intensities(
+            self.pattern_x,
+            self.pattern_intensities,
+            self.window
+        )
+
+        self.possible_dimensions = find_possible_dimensions(len(self.window_intensities))
+
+        if self.dimension is None or self.dimension not in self.possible_dimensions:
+            self.dimension = self.possible_dimensions[0]
+
+        self.map = create_map(self.window_intensities, self.dimension)
+        self.map_changed.emit()
+
+    def integrate(self):
+        self.pattern_x = []
+        self.pattern_intensities = []
+        self.point_infos = []
+
+        for ind, filename in enumerate(self.filenames):
+            self.configuration.img_model.load(filename)
+            frame_ind = 0  # currently we are not handling multi frames yet
+            if ind == 0:
+                x, y = self.configuration.integrate_image_1d()
+                self.pattern_x = x
+            else:
+                _, y = self.configuration.integrate_image_1d()
+            self.point_infos.append(
+                MapPointInfo(
+                    os.path.basename(filename),
+                    frame_ind
+                ))
+
+            self.pattern_intensities.append(y)
+        self.pattern_intensities = np.array(self.pattern_intensities)
+
+    def set_window(self, window: tuple[float, float]):
+        self.window = window
+        self.window_intensities = get_window_intensities(
+            self.pattern_x,
+            self.pattern_intensities,
+            self.window
+        )
+        self.map = create_map(self.window_intensities, self.dimension)
+        self.map_changed.emit()
+
+    def set_dimensions(self, dimension: (float, float)):
+        if dimension not in self.possible_dimensions:
+            return
+        self.dimension = dimension
+        self.map = create_map(self.window_intensities, self.dimension)
+        self.map_changed.emit()
+
+    def get_point_info(self, row_index: float, column_index: float) -> MapPointInfo:
+        ind = column_index + self.dimension[1] * row_index
+        return self.point_infos[ind]
+
+
+def get_center_window(x, window_range=3) -> list[float, float]:
+    """
+    Estimates a window of [x_min, x_max] centered in the x value list.
+    :param x: a numpy array
+    :param window_range: the window will be estimated with +- range * x_step
+    :return: windows with [x_min, x_max]
+    """
+    window_center = x[int(len(x) / 2)]
+    x_step = np.mean(np.diff(x))
+    return [window_center - window_range * x_step, window_center + window_range * x_step]
+
+
+def ind_in_window(x_array, window: (float, float)) -> np.ndarray:
+    """
+    Gets the indices of a numpy array which are in the window
+    :param x_array: a numpy array
+    :param window: tuple/list of lower value and upper value of the window
+    :return: list of indices
+    """
+    return np.where((x_array > window[0]) & (x_array < window[1]))[0]
+
+
+def get_window_intensities(pattern_x, intensities, window: (float, float)) -> np.ndarray:
+    """
+    Estimates the intensities inside the specified window
+    :param pattern_x: a numpy array of x values from the pattern
+    :param intensities: a 2D numpy array holding the intensities of all patterns
+    :param window: tuple/list of lower value and upper value of the summing window
+    :return: an 1D array containing the sum of  intensities inside the window for each pattern
+    """
+    indices = ind_in_window(pattern_x, window)
+    return np.sum(intensities[:, indices], axis=1)
+
+
+def find_possible_dimensions(num_points: int) -> list[(int, int)]:
+    """
+    Finds the possible dimension for a map with a given number of points
+    :param num_points: number of points for the map
+    :return: list of dimension pairs (x-dimension, y-dimension)
+    """
+    dimension_pairs = []
+    for n in range(1, int(np.floor(np.sqrt(num_points + 1))) + 1):
+        if num_points % n == 0:
+            dim1 = n
+            dim2 = num_points // n
+            dimension_pairs.append((dim1, dim2))
+            if dim1 != dim2:
+                dimension_pairs.append((dim2, dim1))
+    dimension_pairs.sort(key=lambda x: ((x[0] + x[1]) / 2 - np.sqrt(num_points)) ** 2)
+    return dimension_pairs
+
+
+def create_map(data: np.ndarray, dimension: (int, int)) -> np.ndarray:
+    """
+    Creates a new map from the given 1D array and specified dimension. It will
+    always create a copy of the data.
+    :param data: input data for creating the map
+    :param dimension: tuple of integers giving the shape of the map
+    :return: numpy array with the reorganized data
+    """
+    new_data = np.copy(data)
+    return np.reshape(new_data, dimension)
