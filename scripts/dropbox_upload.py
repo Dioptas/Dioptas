5088704b7417dd95973f364451d2816f82fa8b96
Clemens Prescher <clemens.prescher@gmail.com>
2023-09-28 11:32:00 +0200

    use Github artifacts for all CD, disable dropbox

--- scripts/dropbox_upload.py
+++ scripts/dropbox_upload.py
@@ -13,10 +13,5 @@ if os.getenv('DROPBOX_TOKEN') == None:
     exit(-1)
 
 db = Dropbox(os.getenv('DROPBOX_TOKEN'))
-# delete file if it already exists in dropbox
-try:
-    db.files_delete(sys.argv[2] + '/' + sys.argv[1])
-except:
-    pass
 db.files_upload(open(sys.argv[1], 'rb').read(), sys.argv[2])
 print('{} successfully uploaded to dropbox folder {}'.format(sys.argv[1], sys.argv[2]))
