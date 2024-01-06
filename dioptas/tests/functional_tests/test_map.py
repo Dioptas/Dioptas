08e95f50ab49f669d3537ae8a34a497d100cd07f
Clemens Prescher <clemens.prescher@gmail.com>
2024-01-06 15:07:25 +0100

    loose connection of image and pattern widget to configuration

--- dioptas/tests/functional_tests/test_map.py
+++ dioptas/tests/functional_tests/test_map.py
@@ -27,10 +27,14 @@ from ..utility import click_button
 
 unittest_path = os.path.dirname(__file__)
 data_path = os.path.join(unittest_path, os.pardir, "data")
-map_img_path = os.path.join(data_path, 'map')
-map_pattern_path = os.path.join(data_path, 'map', 'xy')
-map_img_file_names = [f for f in os.listdir(map_img_path) if os.path.isfile(os.path.join(map_img_path, f))]
-map_img_file_paths = [os.path.join(map_img_path, filename) for filename in map_img_file_names]
+map_img_path = os.path.join(data_path, "map")
+map_pattern_path = os.path.join(data_path, "map", "xy")
+map_img_file_names = [
+    f for f in os.listdir(map_img_path) if os.path.isfile(os.path.join(map_img_path, f))
+]
+map_img_file_paths = [
+    os.path.join(map_img_path, filename) for filename in map_img_file_names
+]
 
 
 def mock_open_filenames(filepaths):
@@ -40,11 +44,13 @@ def mock_open_filenames(filepaths):
 def test_map(main_controller):
     # Herbert has recently collected a large map of his sample and wants to visualize
     # and explore it in Dioptas.
-    
 
     # He opens Dioptas loads his calibration file and is curious to see that there
     # is a mode on the left side which is called "Map".
-    main_controller.model.current_configuration.calibration_model.load(os.path.join(data_path, 'CeO2_Pilatus1M.poni'))
+    main_controller.model.current_configuration.img_model.load(map_img_file_paths[0])
+    main_controller.model.current_configuration.calibration_model.load(
+        os.path.join(data_path, "CeO2_Pilatus1M.poni")
+    )
 
     # He clicks on the map mode and sees a beautiful gui with 2 image views and one pattern view.
 
@@ -68,7 +74,20 @@ def test_map(main_controller):
     # generates a map of something on the left side of the GUI.
 
     assert map_widget.control_widget.file_list.count() == len(map_img_file_names)
-    assert map_widget.map_plot_widget.img_item.image is not None
+    assert map_widget.map_plot_widget.img_data is not None
+
+    # He notices that the first image is loaded in the corresponding image view
+    # and the corresponding pattern is shown in the pattern view.
+
+    assert map_widget.img_plot_widget.img_data is not None
+    assert map_widget.pattern_plot_widget.plot_item.getData() is not None
+
+    # He clicks on the file list in the control widget, and sees, that the image and pattern
+    # is updated in the corresponding views.
+
+    map_widget.control_widget.file_list.setCurrentRow(1)
+    loaded_filename = main_controller.model.current_configuration.img_model.filename
+    assert loaded_filename == map_img_file_paths[1]
 
     # He clicks on individual points of the map and sees that the corresponding image and pattern
     # is shown in the right side of the GUI.
