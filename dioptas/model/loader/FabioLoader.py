6de528220736e5fd45c5ca2524865628ee1bf7ec
Clemens Prescher <clemens.prescher@gmail.com>
2026-03-25 07:17:40 +0100

    refactor: add type hints to FabioLoader.py

--- dioptas/model/loader/FabioLoader.py
+++ dioptas/model/loader/FabioLoader.py
@@ -1,17 +1,16 @@
 # SPDX-License-Identifier: MIT
 
+from __future__ import annotations
+
 import fabio
+import numpy as np
 
 
 class FabioLoader:
-    def __init__(self, filename):
-        """
-        Loads an Hdf5 image produced by ESRF
-        :param filename: path to the hdf5 file to be loaded
-        """
+    def __init__(self, filename: str) -> None:
+        """Loads an image file using the fabio library."""
+        self.fabio_image: fabio.fabioimage.FabioImage = fabio.open(filename)
+        self.series_max: int = self.fabio_image.nframes
 
-        self.fabio_image = fabio.open(filename)
-        self.series_max = self.fabio_image.nframes
-
-    def get_image(self, ind=0):
+    def get_image(self, ind: int = 0) -> np.ndarray:
         return self.fabio_image.get_frame(ind).data[::-1]
